9a4422ce790a51a18d1ea092591ce31adfbd139a
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-03 22:26:02 +0800

    Update webweb.sh

--- LIENOL/files/etc/webweb.sh
+++ LIENOL/files/etc/webweb.sh
@@ -6,6 +6,8 @@ if [ -n "$(ls -A "/etc/closedhcp" 2>/dev/null)" ]; then
   sed -i '/leasetime/d' /etc/config/dhcp
   rm -rf /etc/closedhcp
 fi
+sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
+echo "DISTRIB_REVISION='19.07'" >> /etc/openwrt_release
 source /etc/openwrt_release
 sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
