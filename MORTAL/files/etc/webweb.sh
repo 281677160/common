5d0967d3492db771b448bb1d98d57fb9e45ec3c4
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-03 19:54:09 +0800

    Update webweb.sh

--- MORTAL/files/etc/webweb.sh
+++ MORTAL/files/etc/webweb.sh
@@ -2,5 +2,5 @@
 
 sed -i '/coremark.sh/d' /etc/crontabs/root 2>/dev/null
 source /etc/openwrt_release
-sed -i "s/x86_64/${DISTRIB_ARCH}/g" /etc/banner
+sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 rm -rf /etc/webweb.sh
