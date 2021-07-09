f7106255b53d68b4049e63067bb38f41448bf53b
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-09 15:20:30 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -299,6 +299,8 @@ elif [[ `grep -c "KERNEL_PATCHVER=" target/linux/${TARGET_BOARD}/Makefile` -eq '
 else
 	PATCHVER=unknown
 fi
+KERNEL_PATCHVER=$(egrep -o "${PATCHVER}.[0-9]+" include/kernel-version.mk)
+[[ -z ${KERNEL_PATCHVER} ]] && KERNEL_PATCHVER=unknown
 }
 
 
@@ -385,7 +387,7 @@ TIME b "编译源码: ${CODE}"
 TIME b "源码链接: ${REPO_URL}"
 TIME b "源码分支: ${REPO_BRANCH}"
 TIME b "源码作者: ${ZUOZHE}"
-TIME b "默认内核: ${PATCHVER}"
+TIME b "默认内核: ${KERNEL_PATCHVER}"
 TIME b "Luci版本: ${OpenWrt_name}"
 [[ "${Modelfile}" == "openwrt_amlogic" ]] && {
 	TIME b "编译机型: ${TARGET_model}"
