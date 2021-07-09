3c0ed4de399070a0fc8a4752343f686a0bda825a
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-09 14:52:54 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -385,7 +385,7 @@ TIME b "编译源码: ${CODE}"
 TIME b "源码链接: ${REPO_URL}"
 TIME b "源码分支: ${REPO_BRANCH}"
 TIME b "源码作者: ${ZUOZHE}"
-TIME b "内核版本: ${PATCHVER}"
+TIME b "默认内核: ${PATCHVER}"
 TIME b "Luci版本: ${OpenWrt_name}"
 [[ "${Modelfile}" == "openwrt_amlogic" ]] && {
 	TIME b "编译机型: ${TARGET_model}"
