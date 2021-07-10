1e17d8639097f5807b8c6a0819e3b4fa5ef2f316
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-11 01:16:51 +0800

    Update webweb.sh

--- LEDE/diy/package/base-files/files/bin/webweb.sh
+++ LEDE/diy/package/base-files/files/bin/webweb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-source /etc/openwrt_release
-sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
-
 sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
 sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
 
@@ -10,6 +7,11 @@ sed -i 's/<a href/<!--<a href/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 sed -i 's/%>)<\/a> \//%>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 
 [[ ! -f /mnt/network ]] && chmod +x /bin/networkip && source /bin/networkip
+
+if [[ `grep -c "x86" /etc/openwrt_release` -eq '0' ]]; then
+  source /etc/openwrt_release
+  sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
+fi
 
 rm -rf /bin/networkip
 rm -rf /bin/webweb.sh
