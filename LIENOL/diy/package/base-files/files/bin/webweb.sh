3a5b2bdd0112a67343f2a799d1fdf359ebad43f4
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-11 01:16:15 +0800

    Update webweb.sh

--- LIENOL/diy/package/base-files/files/bin/webweb.sh
+++ LIENOL/diy/package/base-files/files/bin/webweb.sh
@@ -3,9 +3,6 @@
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
 echo "DISTRIB_REVISION='19.07'" >> /etc/openwrt_release
 
-source /etc/openwrt_release
-sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
-
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
 
@@ -15,6 +12,11 @@ sed -i 's/<a href/<!--<a href/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 sed -i 's/%>)<\/a> \//%>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 
 [[ ! -f /mnt/network ]] && chmod +x /bin/networkip && source /bin/networkip
+
+if [[ `grep -c "x86" /etc/openwrt_release` -eq '0' ]]; then
+  source /etc/openwrt_release
+  sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
+fi
 
 rm -rf /bin/networkip
 rm -rf /bin/webweb.sh
