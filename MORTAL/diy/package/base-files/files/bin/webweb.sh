ca07b63b2a0910b039368eb77849206795d187b3
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-10 13:09:15 +0800

    Create webweb.sh

---
+++ MORTAL/diy/package/base-files/files/bin/webweb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sed -i '/coremark.sh/d' /etc/crontabs/root 2>/dev/null
+
+source /etc/openwrt_release
+sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
+
+sed -i 's/<a href/<!--<a href/g' /usr/lib/lua/luci/view/themes/*/footer.htm
+sed -i 's/%>)<\/a> \//%>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
+
+[[ ! -f /mnt/network ]] && chmod +x /bin/networkip && source /bin/networkip
+
+rm -rf /bin/networkip
+rm -rf /bin/webweb.sh
+exti 0
