0b449bef2b715de5e7d7c5a323b697a9accb2322
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-11 21:10:45 +0800

    Update webweb.sh

--- LIENOL/diy/package/base-files/files/bin/webweb.sh
+++ LIENOL/diy/package/base-files/files/bin/webweb.sh
@@ -13,12 +13,12 @@ sed -i 's/%>)<\/a> \//%>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.h
 
 [[ ! -f /mnt/network ]] && chmod +x /bin/networkip && source /bin/networkip
 
+cp -Rf /etc/config/network /mnt/network
+
 if [[ `grep -c "x86" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 fi
-
-cp -Rf /etc/config/network /mnt/network
 
 rm -rf /bin/networkip
 rm -rf /bin/webweb.sh
