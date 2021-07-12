f388172886c7f07703b5e9c70c00f78d1d79d4c0
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-12 11:39:01 +0800

    Update webweb.sh

--- MORTAL/diy/package/base-files/files/etc/webweb.sh
+++ MORTAL/diy/package/base-files/files/etc/webweb.sh
@@ -5,7 +5,7 @@ sed -i '/coremark.sh/d' /etc/crontabs/root
 sed -i 's/<a href/<!--<a href/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 sed -i 's/%>)<\/a> \//%>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 
-[[ ! -f /mnt/network ]] && chmod +x /bin/networkip && source /bin/networkip
+[[ ! -f /mnt/network ]] && chmod +x /etc/networkip && source /etc/networkip
 
 cp -Rf /etc/config/network /mnt/network
 
@@ -14,6 +14,6 @@ if [[ `grep -c "x86" /etc/openwrt_release` -eq '0' ]]; then
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 fi
 
-rm -rf /bin/networkip
+rm -rf /etc/networkip
 rm -rf /etc/webweb.sh
 exit 0
