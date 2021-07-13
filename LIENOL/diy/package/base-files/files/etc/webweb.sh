1bc53dcbad51897c5c21e956043a01f044c94c41
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-13 15:00:15 +0800

    Update webweb.sh

--- LIENOL/diy/package/base-files/files/etc/webweb.sh
+++ LIENOL/diy/package/base-files/files/etc/webweb.sh
@@ -19,7 +19,9 @@ if [[ `grep -c "x86" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 fi
+
 chmod +x /etc/adgup && bash /etc/adgup
+
 rm -rf /etc/networkip
 rm -rf /etc/webweb.sh
 exit 0
