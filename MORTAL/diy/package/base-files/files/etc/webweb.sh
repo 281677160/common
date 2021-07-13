c4c9f1f0826262848f0db5b5d375fe3668500cc5
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-13 14:58:17 +0800

    Update webweb.sh

--- MORTAL/diy/package/base-files/files/etc/webweb.sh
+++ MORTAL/diy/package/base-files/files/etc/webweb.sh
@@ -13,6 +13,9 @@ if [[ `grep -c "x86" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 fi
+if [[ -e /usr/share/AdGuardHome ]] && [[ -e /etc/init.d/AdGuardHome ]]; then
+ chmod -R +x /usr/share/AdGuardHome /etc/init.d/AdGuardHome
+fi
 
 rm -rf /etc/networkip
 rm -rf /etc/webweb.sh
