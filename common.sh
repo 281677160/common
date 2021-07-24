e460210d08f1590443f27d1c9e5105e1a7902e2f
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-24 22:00:08 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -501,7 +501,7 @@ if [[ ${REGULAR_UPDATE} == "true" ]]; then
 	TIME g "《编译成功，会自动把固件发布到指定地址，然后才会生成云端路径》"
 	TIME g "《普通的那个发布固件跟云端的发布路径是两码事，如果你不需要普通发布的可以不用打开发布功能》"
 	TIME g "《请把“REPO_TOKEN”密匙设置好,没设置好密匙运行到 “把定时更新固件发布到云端” 步骤就会出错,生成不了云端地址》"
-	TIME g "《设置密匙教程： https://github.com/danshui-git/shuoming/blob/master/jm.md》"
+	TIME g "《设置密匙教程： https://github.com/danshui-git/shuoming/blob/master/jm.md 》"
 	echo
 else
 	echo
