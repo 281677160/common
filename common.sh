b56c3029c9d284712d4e7c22cf2ee2962830ba5c
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-26 15:24:49 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -88,7 +88,7 @@ sed -i "/exit 0/i\chmod +x /etc/webweb.sh && source /etc/webweb.sh" $ZZZ
 Diy_mortal() {
 
 find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-light' -o -name 'luci-app-fileassistant'  | xargs -i rm -rf {}
-find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-openwrt' | xargs -i rm -rf {}
+find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-openwrt' -o -name 'luci-app-cifs' | xargs -i rm -rf {}
 }
 
 
