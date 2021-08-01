1a89c18945f32cb32b1706b89e2f2112fdbd2204
281677160 <66651997+281677160@users.noreply.github.com>
2021-08-02 00:28:07 +0800

    Update webweb.sh

--- MORTAL/diy/package/base-files/files/etc/webweb.sh
+++ MORTAL/diy/package/base-files/files/etc/webweb.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-sleep 3
-
 sed -i 's/<a href/<!--<a href/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 sed -i 's/%>)<\/a> \//%>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 
 sed -i '/coremark.sh/d' /etc/crontabs/root
 
 [[ ! -f /mnt/network ]] && chmod +x /etc/networkip && source /etc/networkip
-
-cp -Rf /etc/config/network /mnt/network
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
@@ -19,6 +15,10 @@ fi
 if [[ -e /usr/share/AdGuardHome ]] && [[ -e /etc/init.d/AdGuardHome ]]; then
  chmod -R +x /usr/share/AdGuardHome /etc/init.d/AdGuardHome
 fi
+
+cp -Rf /etc/config/network /mnt/network
+
+chmod -R +x /etc/init.d
 
 rm -rf /etc/networkip
 rm -rf /etc/webweb.sh
