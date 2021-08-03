9b28aa9d53a39686806bb9368b1947372c2a3829
281677160 <66651997+281677160@users.noreply.github.com>
2021-08-03 20:24:57 +0800

    Update AutoUpdate.sh

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -217,9 +217,9 @@ TIME b "检测网络环境中,请稍后..."
 if [[ "$(cat ${Download_Path}/Installed_PKG_List)" =~ curl ]];then
 	export Google_Check=$(curl -I -s --connect-timeout 8 google.com -w %{http_code} | tail -n1)
 	if [ ! "$Google_Check" == 301 ];then
-		TIME z "警告：梯子翻墙失败,或许有可能会获取不了云端固件版本信息!"
+		TIME z "警告：google连接失败,或许有可能会获取不了云端固件版本信息!"
 	else
-		TIME y "网络检测成功,您的梯子翻墙成功！"
+		TIME y "google连接成功！"
 	fi
 fi
 [[ -z ${CURRENT_Version} ]] && TIME r "本地固件版本获取失败,请检查/bin/openwrt_info文件的值!" && exit 1
