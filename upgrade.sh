8d4c6187ff8864c94efad985bab8560372d4c338
281677160 <66651997+281677160@users.noreply.github.com>
2021-08-07 20:14:54 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -65,6 +65,7 @@ GET_TARGET_INFO() {
 	Github_UP_RELEASE="${Github}/releases/AutoUpdate"
 	Openwrt_Version="${REPO_Name}-${TARGET_PROFILE}-${Compile_Date}"
 	Egrep_Firmware="${LUCI_Name}-${REPO_Name}-${TARGET_PROFILE}"
+	Ghproxy_Tags="${REPO_Name}-${TARGET_PROFILE}"
 }
 
 Diy_Part1() {
@@ -89,6 +90,7 @@ Diy_Part2() {
 	Download_Path=/tmp/Downloads
 	Version=${AutoUpdate_Version}
 	Download_Tags=/tmp/Downloads/Github_Tags
+	Ghproxy_Tags=${Ghproxy_Tags}
 	EOF
 }
 
@@ -133,7 +135,7 @@ Diy_Part3() {
 	;;
 	esac
 	cd ${Home}
-	ls ${Home}/bin/Firmware > ${Home}/bin/Firmware/${TARGET_PROFILE}
+	ls ${Home}/bin/Firmware > ${Home}/bin/Firmware/${Ghproxy_Tags}
 }
 
 Mkdir() {
