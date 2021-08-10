6ced5965114d39798c1f5abcd76830ff4f7ad735
281677160 <66651997+281677160@users.noreply.github.com>
2021-08-10 11:13:56 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -134,7 +134,6 @@ fi
 if [ -n "$(ls -A "feeds/luci/applications/luci-app-rebootschedule" 2>/dev/null)" ]; then
 	chmod -R 775 feeds/luci/applications/luci-app-rebootschedule
 fi
-mkdir -p files/etc/hotplug.d/block && curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/block/10-mount > files/etc/hotplug.d/block/10-mount
 }
 
 ################################################################################################################
@@ -289,6 +288,10 @@ Diy_chuli() {
 if [[ "${TARGET_BOARD}" == "x86" ]]; then
 	cp -Rf "${Home}"/build/common/Custom/DRM-I915 target/linux/x86/DRM-I915
 	for X in $(ls -1 target/linux/x86 | grep "config-"); do echo -e "\n$(cat target/linux/x86/DRM-I915)" >> target/linux/x86/${X}; done
+fi
+
+if [[ `grep -c "CONFIG_PACKAGE_ntfs-3g=y" ${Home}/.config` -eq '1' ]]; then
+	mkdir -p files/etc/hotplug.d/block && curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/block/10-mount > files/etc/hotplug.d/block/10-mount
 fi
 
 if [[ "${Modelfile}" == "openwrt_amlogic" ]]; then
