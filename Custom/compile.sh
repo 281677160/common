bbcb9c6cbe2c6674a6f0b7474771840faa374c32
281677160 <66651997+281677160@users.noreply.github.com>
2021-08-20 15:58:55 +0800

    Update compile.sh

--- Custom/compile.sh
+++ Custom/compile.sh
@@ -462,8 +462,9 @@ if [ -n "$(ls -A "${Home}/Chajianlibiao" 2>/dev/null)" ]; then
 	source ${Home}/CHONGTU
 	rm -rf {CHONGTU,Chajianlibiao}
 	echo
-	TIME g "如需重新编译请按 Ctrl+c 结束此次编译，继续使用命令重新编译，否则20秒后继续编译!"
-	sleep 20s
+	TIME g "如需重新编译请按 Ctrl+c 结束此次编译，继续使用命令重新编译，否则30秒后继续编译!"
+	make defconfig
+	sleep 30s
 fi
 cp -rf ${Home}/.config ${Home}/.bf_config
 TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)"
