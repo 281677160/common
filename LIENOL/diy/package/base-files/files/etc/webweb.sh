386686855fa5b3ad39d72359a1faef88f3123a21
281677160 <66651997+281677160@users.noreply.github.com>
2021-09-14 19:43:32 +0800

    Update webweb.sh

--- LIENOL/diy/package/base-files/files/etc/webweb.sh
+++ LIENOL/diy/package/base-files/files/etc/webweb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='19.07'" >> /etc/openwrt_release
+echo "DISTRIB_REVISION='Lienol-19.07'" >> /etc/openwrt_release
 
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
