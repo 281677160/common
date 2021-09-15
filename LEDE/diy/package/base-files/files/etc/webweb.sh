afefbbe7077571db173d3323c7cb0c29eb80acce
281677160 <66651997+281677160@users.noreply.github.com>
2021-09-15 23:26:20 +0800

    Update webweb.sh

--- LEDE/diy/package/base-files/files/etc/webweb.sh
+++ LEDE/diy/package/base-files/files/etc/webweb.sh
@@ -7,6 +7,8 @@ sed -i 's/luciversion %>)<\/a> \//luciversion %>)<\/a> \/-->/g' /usr/lib/lua/luc
 
 cp -Rf /etc/config/network /mnt/network
 
+echo "0 2 1 * * rm /tmp/luci-indexcache > /dev/null 2>&1" >> /etc/crontabs/root
+
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
