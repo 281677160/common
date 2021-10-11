3d57bf79ac5e6c8ea6f31e1eb5cfe58be316fd92
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-11 20:18:04 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -270,17 +270,6 @@ if [[ `grep -c "CONFIG_PACKAGE_dnsmasq-full=y" ${Home}/.config` -eq '1' ]]; then
 	if [[ `grep -c "CONFIG_PACKAGE_dnsmasq=y" ${Home}/.config` -eq '1' ]] || [[ `grep -c "CONFIG_PACKAGE_dnsmasq-dhcpv6=y" ${Home}/.config` -eq '1' ]]; then
 		sed -i 's/CONFIG_PACKAGE_dnsmasq=y/# CONFIG_PACKAGE_dnsmasq is not set/g' ${Home}/.config
 		sed -i 's/CONFIG_PACKAGE_dnsmasq-dhcpv6=y/# CONFIG_PACKAGE_dnsmasq-dhcpv6 is not set/g' ${Home}/.config
-		echo "TIME r \"测试测试\"" >>CHONGTU
-		echo "TIME z \"\"" >>CHONGTU
-		echo "TIME b \"插件冲突信息\"" > ${Home}/Chajianlibiao
-	fi
-fi
-if [[ `grep -c "CONFIG_PACKAGE_dnsmasq-full=y" ${Home}/.config` -eq '1' ]]; then
-	if [[ `grep -c "CONFIG_PACKAGE_dnsmasq-dhcpv6=y" ${Home}/.config` -eq '1' ]]; then
-		sed -i 's/CONFIG_PACKAGE_dnsmasq-dhcpv6=y/# CONFIG_PACKAGE_dnsmasq-dhcpv6 is not set/g' ${Home}/.config
-		echo "TIME r \"测试测试2\"" >>CHONGTU
-		echo "TIME z \"\"" >>CHONGTU
-		echo "TIME b \"插件冲突信息\"" > ${Home}/Chajianlibiao
 	fi
 fi
 if [[ `grep -c "CONFIG_PACKAGE_luci-app-samba4=y" ${Home}/.config` -eq '1' ]]; then
