91b77854b7796836997ca8638e3d5c9d432c4581
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-22 15:27:55 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -86,7 +86,7 @@ sed -i "/exit 0/i\chmod +x /etc/webweb.sh && source /etc/webweb.sh" $ZZZ
 ################################################################################################################
 # 天灵源码18.06 diy.sh文件
 ################################################################################################################
-Diy_mortal() {
+Diy_Tianling() {
 
 find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-light' | xargs -i rm -rf {}
 find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-openwrt' -o -name 'luci-app-cifs' | xargs -i rm -rf {}
