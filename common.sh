ec9e3fde158e78990ce9ef3906aa5bafd10b5b9d
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-27 18:05:53 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -28,7 +28,7 @@ Compte=$(date +%Y年%m月%d号%H时%M分)
 Diy_lede() {
 find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-argon' -o -name 'mentohust' | xargs -i rm -rf {}
 find . -name 'luci-app-ipsec-vpnd' -o -name 'k3screenctrl' -o -name 'luci-app-wol' | xargs -i rm -rf {}
-find . -name 'luci-app-wrtbwmon' -o -name 'wrtbwmon' -o -name 'luci-app-unblockneteasemusic' | xargs -i rm -rf {}
+find . -name 'luci-app-wrtbwmon' -o -name 'wrtbwmon' -o -name 'luci-app-unblockmusic' | xargs -i rm -rf {}
 find . -name 'UnblockNeteaseMusic-Go' -o -name 'UnblockNeteaseMusic' -o -name 'acme' -o -name 'luci-app-acme' | xargs -i rm -rf {}
 
 sed -i '/to-ports 53/d' $ZZZ
