4ba36c4c147522d465f5c5409588159ac7e3aab8
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-28 20:04:43 +0800

    Update webweb.sh

--- LIENOL/diy/package/base-files/files/etc/webweb.sh
+++ LIENOL/diy/package/base-files/files/etc/webweb.sh
@@ -25,6 +25,11 @@ if [[ -e /usr/share/AdGuardHome ]] && [[ -e /etc/init.d/AdGuardHome ]]; then
  chmod -R +x /usr/share/AdGuardHome /etc/init.d/AdGuardHome
 fi
 
+if [[ ! -e /usr/bin/AdGuardHome ]]; then
+rm -fr /etc/config/AdGuardHome.yaml
+rm -fr /etc/AdGuardHome.yaml
+fi
+
 chmod -R +x /etc/init.d /usr/share
 
 /etc/init.d/uhttpd restart
