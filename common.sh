93da4b5a0133988a5142849e6a232eea8ce9a4ac
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-30 17:25:22 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -498,21 +498,21 @@ rm -rf ${Home}/files/{README,README.md}
 # 整理不要的固件或者文件
 ################################################################################################################
 Diy_Arrange() {
-[[ -n "${CLEAR1}" ]] && find ${Home}/bin/targets -name "*${CLEAR1}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR2}" ]] && find ${Home}/bin/targets -name "*${CLEAR2}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR3}" ]] && find ${Home}/bin/targets -name "*${CLEAR3}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR4}" ]] && find ${Home}/bin/targets -name "*${CLEAR4}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR5}" ]] && find ${Home}/bin/targets -name "*${CLEAR5}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR6}" ]] && find ${Home}/bin/targets -name "*${CLEAR6}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR7}" ]] && find ${Home}/bin/targets -name "*${CLEAR7}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR8}" ]] && find ${Home}/bin/targets -name "*${CLEAR8}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR9}" ]] && find ${Home}/bin/targets -name "*${CLEAR9}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR10}" ]] && find ${Home}/bin/targets -name "*${CLEAR10}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR11}" ]] && find ${Home}/bin/targets -name "*${CLEAR11}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR12}" ]] && find ${Home}/bin/targets -name "*${CLEAR12}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR13}" ]] && find ${Home}/bin/targets -name "*${CLEAR13}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR14}" ]] && find ${Home}/bin/targets -name "*${CLEAR14}*" | xargs -i rm -rf {}
-[[ -n "${CLEAR15}" ]] && find ${Home}/bin/targets -name "*${CLEAR15}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR1}" ]] && find . -name "*${CLEAR1}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR2}" ]] && find . -name "*${CLEAR2}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR3}" ]] && find . -name "*${CLEAR3}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR4}" ]] && find . -name "*${CLEAR4}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR5}" ]] && find . -name "*${CLEAR5}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR6}" ]] && find . -name "*${CLEAR6}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR7}" ]] && find . -name "*${CLEAR7}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR8}" ]] && find . -name "*${CLEAR8}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR9}" ]] && find . -name "*${CLEAR9}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR10}" ]] && find . -name "*${CLEAR10}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR11}" ]] && find . -name "*${CLEAR11}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR12}" ]] && find . -name "*${CLEAR12}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR13}" ]] && find . -name "*${CLEAR13}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR14}" ]] && find . -name "*${CLEAR14}*" | xargs -i rm -rf {}
+[[ -n "${CLEAR15}" ]] && find . -name "*${CLEAR15}*" | xargs -i rm -rf {}
 }
 
 ################################################################################################################
