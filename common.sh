6ba24caa2b24984f70417f7dcda63cf5817f2c51
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-30 22:46:08 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -49,7 +49,7 @@ if [[ "${Modelfile}" == "openwrt_amlogic" ]]; then
 	brcmfmac-firmware-43430-sdio brcmfmac-firmware-43455-sdio kmod-brcmfmac wpad \
 	kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs ntfs-3g \
 	kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
-	kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
+	kmod-usb-net \
 	blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip \
 	lscpu htop iperf3 curl lm-sensors python3 luci-app-amlogic
 	"
