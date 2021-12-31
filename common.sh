ede5014b6ca8cef74b62f2d8e85641469213b767
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-31 16:21:16 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -78,8 +78,8 @@ find . -name 'UnblockNeteaseMusic-Go' -o -name 'UnblockNeteaseMusic' -o -name 'l
 rm -rf feeds/packages/libs/libcap
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 rm -rf package/luci-app-passwall/{v2ray-core,v2ray-plugin,xray-core,xray-plugin}
-git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
-# git clone https://github.com/281677160/ssr package/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/281677160/ssr package/luci-app-ssr-plus
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += luci-app-passwall/g' target/linux/x86/Makefile
 sed -i "/exit 0/i\chmod +x /etc/webweb.sh && source /etc/webweb.sh" $ZZZ
