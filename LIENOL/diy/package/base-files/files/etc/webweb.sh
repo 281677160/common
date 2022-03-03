8da8858c2c172161a26e617b17f5862ee9d532df
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-03 16:30:34 +0800

    Update webweb.sh

--- LIENOL/diy/package/base-files/files/etc/webweb.sh
+++ LIENOL/diy/package/base-files/files/etc/webweb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='Lienol-19.07'" >> /etc/openwrt_release
-
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
 
@@ -10,7 +7,7 @@ sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /u
 
 cp -Rf /etc/config/network /mnt/network
 
-echo "0 1 * * 1 rm /tmp/luci-indexcache > /dev/null 2>&1" >> /etc/crontabs/root
+echo "0 1 * * 1 rm /tmp/luci-*cache > /dev/null 2>&1" >> /etc/crontabs/root
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
@@ -21,7 +18,7 @@ if [[ -e /usr/share/AdGuardHome ]] && [[ -e /etc/init.d/AdGuardHome ]]; then
  chmod -R +x /usr/share/AdGuardHome /etc/init.d/AdGuardHome
 fi
 
-if [[ ! -e /usr/bin/AdGuardHome ]]; then
+if [[ ! -f /usr/bin/AdGuardHome ]]; then
 rm -fr /etc/config/AdGuardHome.yaml
 rm -fr /etc/AdGuardHome.yaml
 fi
