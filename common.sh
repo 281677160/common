b6f58993e9d67c29e76233fbe12656e792f677cd
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-06 12:27:02 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -77,6 +77,8 @@ find . -name 'ddns-scripts_aliyun' -o -name 'ddns-scripts_dnspod' -o -name 'luci
 find . -name 'UnblockNeteaseMusic-Go' -o -name 'UnblockNeteaseMusic' -o -name 'luci-app-unblockmusic' | xargs -i rm -rf {}
 sed -i "s/OpenWrt SNAPSHOT/OpenWrt/g" $ZZZ
 
+svn co https://github.com/kenzok8/openwrt-packages/trunk/{luci-app-store,app-store-ui} package/
+
 ## git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/281677160/openwrt-passwall package/luci-app-passwall
 rm -rf package/luci-app-passwall/{v2ray-core,v2ray-plugin,v2ray-geodata,xray-core,xray-plugin}
