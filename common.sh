9a3e9455480264f55b43b5769887118abcfaf1d2
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-20 10:18:55 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -438,7 +438,7 @@ fi
 
 if [[ "${BY_INFORMATION}" == "true" ]]; then
 	Plug_in="$(grep -i 'CONFIG_PACKAGE_luci-app' ${Home}/.config && grep -i 'CONFIG_PACKAGE_luci-theme' ${Home}/.config)"
-	Plug_in2="$(echo "${Plug_in}" | grep -v '^#' |sed '/INCLUDE/d' |sed '/_Transparent_Proxy/d' |sed '/qbittorrent_static/d' |sed 's/CONFIG_PACKAGE_//g' |sed 's/=y//g' |sed 's/^/、/g' |sed 's/$/\"/g' |awk '$0=NR$0' |sed 's/^/TIME g \"     /g')"
+	Plug_in2="$(echo "${Plug_in}" | grep -v '^#' |sed '/INCLUDE/d' |sed '/_Transparent_Proxy/d' |sed '/qbittorrent_static/d' |sed 's/CONFIG_PACKAGE_//g' |sed 's/=y//g' |sed 's/^/、/g' |sed 's/$/\"/g' |awk '$0=NR$0' |sed 's/^/TIME g \"       /g')"
 	echo "${Plug_in2}" >Plug-in
 fi
 rm -rf ${Home}/files/{README,README.md}
