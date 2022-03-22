e7ac7b671e4d4c7c4b1ff70aa6d0ed26b0051ec1
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-22 19:17:29 +0800

    Update ceshi777.sh

--- ceshi777.sh
+++ ceshi777.sh
@@ -139,7 +139,6 @@ if [[ ${REGULAR_UPDATE} == "true" ]]; then
 	[[ -f "${PATH1}/replace.sh" ]] && cp -Rf "${PATH1}"/replace.sh package/base-files/files/bin/replace.sh
 fi
 [[ -f "$PATH1/openwrt.sh" ]] && cp -Rf $PATH1/openwrt.sh $Home/package/base-files/files/sbin/openwrt
-chmod 777 ${Home}/package/base-files/files/sbin/openwrt
 
 if [[ "${REPO_BRANCH}" == "master" ]]; then
 	cp -Rf "${Home}"/build/common/LEDE/files "${Home}"
