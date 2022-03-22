99accb20fbd6dae0a70715e9d3d7771d71569b0b
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-22 18:42:41 +0800

    Update ceshi777.sh

--- ceshi777.sh
+++ ceshi777.sh
@@ -138,8 +138,8 @@ if [[ ${REGULAR_UPDATE} == "true" ]]; then
 	[[ -f "${PATH1}/AutoUpdate.sh" ]] && cp -Rf "${PATH1}"/AutoUpdate.sh package/base-files/files/bin/AutoUpdate.sh
 	[[ -f "${PATH1}/replace.sh" ]] && cp -Rf "${PATH1}"/replace.sh package/base-files/files/bin/replace.sh
 fi
-[[ -f "${PATH1}/openwrt.sh" ]] && mv -f "${PATH1}"/openwrt.sh package/base-files/files/sbin/openwrt
-chmod 777 package/base-files/files/sbin/openwrt
+[[ -f "${PATH1}/openwrt.sh" ]] && mv -f "${PATH1}/openwrt.sh" "${Home}/package/base-files/files/sbin/openwrt"
+chmod 777 ${Home}/package/base-files/files/sbin/openwrt
 
 if [[ "${REPO_BRANCH}" == "master" ]]; then
 	cp -Rf "${Home}"/build/common/LEDE/files "${Home}"
