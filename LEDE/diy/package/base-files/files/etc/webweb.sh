c21ca2c7b24e438bc5a4ab57f99bff488350ed56
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-24 02:00:36 +0800

    Update webweb.sh

--- LEDE/diy/package/base-files/files/etc/webweb.sh
+++ LEDE/diy/package/base-files/files/etc/webweb.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
+chmod -R 775 /etc/init.d /usr/share
+
 [[ ! -f /mnt/network ]] && chmod +x /etc/networkip && source /etc/networkip
 
 cp -Rf /etc/config/network /mnt/network
 
-if [[ -f /etc/crontabs/root ]]; then
-  sed -i '/mp\/luci-/d' /etc/crontabs/root && echo "0 1 * * 1 rm -rf /tmp/luci-*cache* > /dev/null 2>&1" >> /etc/crontabs/root
-else
-  mkdir -p /etc/crontabs
-  echo "0 1 * * 1 rm -rf /tmp/luci-*cache* > /dev/null 2>&1" > /etc/crontabs/root
-  chmod -R 755 /etc/crontabs
-fi
+sed -i '/mp\/luci-/d' /etc/crontabs/root && echo "0 1 * * 1 rm -rf /tmp/luci-*cache* > /dev/null 2>&1" >> /etc/crontabs/root
 /etc/init.d/cron restart
 
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
-  source /etc/openwrt_release
-  sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
+  DISTRIB_TA="$(grep -i "DISTRIB_TARGET" /etc/openwrt_release |sed "s/'//g" |cut -f2 -d=)"
+  sed -i "s#x86_64#${DISTRIB_TA}#g" /etc/banner
 fi
 
-if [[ -d /usr/share/AdGuardHome ]] && [[ -f /etc/init.d/AdGuardHome ]]; then
- chmod -R 775 /usr/share/AdGuardHome /etc/init.d/AdGuardHome
-else
+if [[ ! -d /usr/share/AdGuardHome ]] && [[ ! -f /etc/init.d/AdGuardHome ]]; then
   rm -fr /etc/config/AdGuardHome.yaml
   rm -fr /etc/AdGuardHome.yaml
 fi
 
-chmod -R 775 /etc/init.d /usr/share
-
 if [[ -f /etc/init.d/ddnsto ]]; then
- chmod 775 /etc/init.d/ddnsto
  /etc/init.d/ddnsto enable
 fi
 
