33812864a74d2a39e667711b7047ae776a5fff88
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-24 09:34:14 +0800

    Update ceshi777.sh

--- ceshi777.sh
+++ ceshi777.sh
@@ -593,7 +593,7 @@ if [[ -f ${GITHUB_WORKSPACE}/Clear ]]; then
   chmod +x ${TARGET_BSGET}/Clear.sh && ${TARGET_BSGET}/Clear.sh
   rm -rf ${TARGET_BSGET}/Clear.sh
 fi
-rename -v "s/^openwrt/${{ env.date1 }}-${{ env.SOURCE }}/" *
+rename -v "s/^openwrt/${{ env.Compile_Date }}-${{ env.SOURCE }}/" *
 
 # 发布用的update_log.txt
 if [ "${UPLOAD_RELEASE}" == "true" ]; then
@@ -628,7 +628,7 @@ TIME b "Luci版本: ${LUCI_EDITION}"
 TIME b "固件作者: ${Author}"
 TIME b "仓库地址: ${Github}"
 TIME b "启动编号: #${Run_number}（${CangKu}仓库第${Run_number}次启动[${Run_workflow}]工作流程）"
-TIME b "编译时间: ${Compte}"
+TIME b "编译时间: ${{ env.Compile_Date }}-${{ env.SOURCE }}"
 [[ "${matrixtarget}" == "openwrt_amlogic" ]] && {
 	TIME g "友情提示：您当前使用【${matrixtarget}】文件夹编译【晶晨系列】固件"
 } || {
