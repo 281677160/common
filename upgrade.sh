146dc0d8da9471f859d786e52ea69dfca86fbd81
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-25 11:01:03 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -84,6 +84,8 @@ function Diy_Part2() {
 	GET_TARGET_INFO
 	cat >${In_Firmware_Info} <<-EOF
 	Github=${Github}
+	Author=${Author}
+	Library=${Library}
 	Warehouse=${Warehouse}
 	SOURCE=${SOURCE}
 	LUCI_EDITION=${LUCI_EDITION}
