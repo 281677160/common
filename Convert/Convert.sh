7c68b69bf999531c84906dc858062bdf6b7b8ca1
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-26 12:40:15 +0800

    Update Convert.sh

--- Convert/Convert.sh
+++ Convert/Convert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp -Rf 99-default-settings package/emortal/default-settings/files/99-default-settings
 cp -Rf SourceCode feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-cp -Rf SourceCode package/emortal/autocore/files/arm/rpcd_10_system.js
-cp -Rf SourceCode package/emortal/autocore/files/x86/rpcd_10_system.js
+cp -Rf SourceCode package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
+cp -Rf SourceCode package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
 chmod 664 feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-chmod 664 package/emortal/autocore/files/arm/rpcd_10_system.js
-chmod 664 package/emortal/autocore/files/x86/rpcd_10_system.js
+chmod 664 package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
+chmod 664 package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
 rm -rf {99-default-settings,SourceCode,Convert.sh,1806-default-settings}
 exit 0
