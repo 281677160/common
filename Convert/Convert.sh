8b28467ffade6767e66ce0a6826c86de68e20e24
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-26 13:13:00 +0800

    Update Convert.sh

--- Convert/Convert.sh
+++ Convert/Convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export Convert_path="$HOME_PATH/build/common/Convert"
-cp -Rf ${Convert_path}/99-default-settings $HOME_PATH/package/emortal/default-settings/files/99-default-settings
+cp -Rf ${Convert_path}/99-default-settings $ZZZ_PATH
 cp -Rf ${Convert_path}/SourceCode $HOME_PATH/feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 cp -Rf ${Convert_path}/SourceCode $HOME_PATH/package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
 cp -Rf ${Convert_path}/SourceCode $HOME_PATH/package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
