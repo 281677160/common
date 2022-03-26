cfc5688f8676c5e49359065afdf7c45fc19bf823
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-26 13:29:21 +0800

    Update Convert.sh

--- Convert/Convert.sh
+++ Convert/Convert.sh
@@ -8,4 +8,3 @@ cp -Rf ${Convert_path}/SourceCode $HOME_PATH/package/emortal/autocore/files/x86/
 chmod 664 $HOME_PATH/feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 chmod 664 $HOME_PATH/package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
 chmod 664 $HOME_PATH/package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
-exit 0
