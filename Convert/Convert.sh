89d7a1dd6c030c3bbfb1c6eb0cc01a900c835221
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-26 12:47:22 +0800

    Update Convert.sh

--- Convert/Convert.sh
+++ Convert/Convert.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cp -Rf 99-default-settings package/emortal/default-settings/files/99-default-settings
-cp -Rf SourceCode feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-cp -Rf SourceCode package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
-cp -Rf SourceCode package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
-chmod 664 feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-chmod 664 package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
-chmod 664 package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
-rm -rf {99-default-settings,SourceCode,Convert.sh,1806-default-settings}
+
+Convert_path="$HOME_PATH/build/common/Convert"
+cp -Rf ${Convert_path}/99-default-settings $HOME_PATH/package/emortal/default-settings/files/99-default-settings
+cp -Rf ${Convert_path}/SourceCode $HOME_PATH/feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+cp -Rf ${Convert_path}/SourceCode $HOME_PATH/package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
+cp -Rf ${Convert_path}/SourceCode $HOME_PATH/package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
+chmod 664 $HOME_PATH/feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+chmod 664 $HOME_PATH/package/emortal/autocore/files/arm/rpcd_10_system.js > /dev/null 2>&1
+chmod 664 $HOME_PATH/package/emortal/autocore/files/x86/rpcd_10_system.js > /dev/null 2>&1
 exit 0
