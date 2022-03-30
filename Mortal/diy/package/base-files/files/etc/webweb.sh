8a1e4217d6b7480eeac144fa22c5c4241a4053df
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-30 23:09:37 +0800

    Update webweb.sh

--- Mortal/diy/package/base-files/files/etc/webweb.sh
+++ Mortal/diy/package/base-files/files/etc/webweb.sh
@@ -11,8 +11,8 @@ sed -i '/mp\/luci-/d' /etc/crontabs/root && echo "0 1 * * 1 rm -rf /tmp/luci-*ca
 
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
-  DISTRIB_TA="$(grep -i "DISTRIB_TARGET" /etc/openwrt_release |sed "s/'//g" |cut -f2 -d=)"
-  sed -i "s#x86_64#${DISTRIB_TA}#g" /etc/banner
+  export DISTRIB_TA="$(grep DISTRIB_TARGET= /etc/openwrt_release |sed "s/'//g" |cut -d "=" -f2)"
+  sed -i "s?x86_64?${DISTRIB_TA}?g" /etc/banner
 fi
 
 if [[ ! -d /usr/share/AdGuardHome ]] && [[ ! -f /etc/init.d/AdGuardHome ]]; then
@@ -27,8 +27,6 @@ fi
 if [[ -f /etc/init.d/ddnsto ]]; then
  /etc/init.d/ddnsto enable
 fi
-
-[[ -f /etc/init.d/adguardhome ]] && rm -rf /etc/init.d/adguardhome
 
 if [[ -d /usr/lib/lua/luci/view/themes/argon ]]; then
   uci set argon.@global[0].bing_background=0
