55f354ff4f88e21b61df7a5e8cef570b5d18f71a
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-01 16:32:33 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -210,7 +210,9 @@ esac
 # 给feeds.conf.default增加插件源
 echo "
 src-git helloworld https://github.com/fw876/helloworld
-src-git passwall https://github.com/281677160/openwrt-passwall
+src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages
+src-git passwall1 https://github.com/xiaorouji/openwrt-passwall;luci
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main
 src-git danshui https://github.com/281677160/openwrt-package.git;${REPO_BRANCH}
 " >> $HOME_PATH/feeds.conf.default
 sed -i '/^\s*$/d' "$HOME_PATH/feeds.conf.default"
