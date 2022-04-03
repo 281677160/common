31b8cccbe847bfcdf366584ae6a0ce28ce2b4bbe
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-03 19:43:51 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -173,7 +173,7 @@ master)
   sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += luci-app-passwall/g' target/linux/*/Makefile
   
   # 修改DISTRIB_DESCRIPTION
-  DISTRIB="$(egrep -o "DISTRIB_DESCRIPTION='.* '" $ZZZ_PATH |sed -r "s/DISTRIB_DESCRIPTION='(.*) '/\1/")"
+  DISTRIB="$(egrep -o "DISTRIB_DESCRIPTION='.*'" $ZZZ_PATH |sed -r "s/DISTRIB_DESCRIPTION='(.*) '/\1/")"
   [[ -n "${DISTRIB}" ]] && sed -i "s?${DISTRIB}?OpenWrt ?g" "$ZZZ_PATH"
 
 ;;
