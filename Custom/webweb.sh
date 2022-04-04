986abb68f2928f602cba56d309d822d87243da89
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-04 16:23:40 +0800

    Update webweb.sh

--- Custom/webweb.sh
+++ Custom/webweb.sh
@@ -15,7 +15,7 @@ uci commit firewall
 cp -Rf /etc/config/network /mnt/network
 
 sed -i '/mp\/luci-/d' /etc/crontabs/root
-echo "*/2 * * * * source /etc/FinishIng.sh > /dev/null 2>&1" >> /etc/crontabs/root
+echo "0 1 * * 1 rm -rf /tmp/luci-*cache* > /dev/null 2>&1" >> /etc/crontabs/root
 /etc/init.d/cron restart
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
