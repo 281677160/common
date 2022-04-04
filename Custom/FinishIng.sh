b51ce0ff06155cd80d2bf6dd855254c9f5f20a79
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-04 16:22:27 +0800

    Update FinishIng.sh

--- Custom/FinishIng.sh
+++ Custom/FinishIng.sh
@@ -2,6 +2,10 @@
 
 sed -i '/coremark/d' /etc/rc.local
 sed -i '/mediaurlbase/d' /etc/rc.local
-sed -i '/FinishIng.sh/d' /etc/crontabs/root
+sed -i '/FinishIng.sh/d' /etc/rc.local
 rm -rf /etc/FinishIng.sh
+if [[ -f '/etc/webweb.sh' ]]; then
+  source /etc/webweb.sh
+  reboot -f
+fi
 exit 0
