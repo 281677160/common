7e09449c6e1025c962118cd07ffe195ddc9e829b
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-19 00:21:22 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -79,6 +79,11 @@ sudo apt-get full-upgrade -y
 sudo apt-get install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
 sudo apt-get autoremove -y --purge
 sudo apt-get clean
+if [[ ${WSL_ubuntu} == "YES" ]] && [[ `dpkg -l | grep -c "daemonize"` == '0' ]]; then
+  sudo apt-get install daemonize
+  sudo daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
+  exec sudo nsenter -t $(pidof systemd) -a su - $LOGNAME
+fi
 sudo timedatectl set-timezone "$TZ"
 if [[ ! ${bendi_script} == "1" ]]; then
   sudo mkdir -p /${matrixtarget}
