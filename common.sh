11edae31b42fcefe2370e8a98699aea9927545d9
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-18 22:55:07 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -74,11 +74,11 @@ echo "正在执行：随便判断一下是不是缺少文件了"
 
 function Diy_update() {
 sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /usr/lib/jvm /opt/ghc
-sudo -E apt-get -qq update -y
-sudo -E apt-get -qq full-upgrade -y
-sudo -E apt-get -qq install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
-sudo -E apt-get -qq autoremove -y --purge
-sudo -E apt-get -qq clean
+sudo apt-get update -y
+sudo apt-get full-upgrade -y
+sudo apt-get install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
+sudo apt-get autoremove -y --purge
+sudo apt-get clean
 sudo timedatectl set-timezone "$TZ"
 if [[ ! ${bendi_script} == "1" ]]; then
   sudo mkdir -p /${matrixtarget}
