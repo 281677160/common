0b95a86e27b7f0a8c2c7276a9ce46fbda333e81a
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-18 17:53:20 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -79,8 +79,8 @@ sudo -E apt-get -qq full-upgrade -y
 sudo -E apt-get -qq install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
 sudo -E apt-get -qq autoremove -y --purge
 sudo -E apt-get -qq clean
-sudo timedatectl set-timezone "$TZ"
 if [[ ! ${bendi_script} == "1" ]]; then
+  sudo timedatectl set-timezone "$TZ"
   sudo mkdir -p /${matrixtarget}
   sudo chown $USER:$GROUPS /${matrixtarget}
 fi
