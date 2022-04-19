4ee2646695a4967f7268a7f87c177c32f7192659
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-19 16:35:41 +0800

    Update AutoUpdate.sh

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -451,6 +451,7 @@ else
 fi
 ' >/mnt/Detectionnetwork
   sed -i '/^$/d' "/mnt/Detectionnetwork"
+  chmod 755 "/mnt/Detectionnetwork"
   echo "*/3* * * * source /mnt/Detectionnetwork > /dev/null 2>&1" >> /etc/crontabs/root
   /etc/init.d/cron restart
   
