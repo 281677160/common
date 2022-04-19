8c3be35cb0a810f760b688cf74f1c2af20d20f41
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-19 23:01:32 +0800

    Update FinishIng.sh

--- Custom/FinishIng.sh
+++ Custom/FinishIng.sh
@@ -10,6 +10,6 @@ if [[ -f '/mnt/network' ]]; then
 fi
 sed -i '/coremark/d' /etc/rc.local
 sed -i '/mediaurlbase/d' /etc/rc.local
-rm -rf /etc/config/FinishIng.sh
+rm -rf /etc/FinishIng.sh
 
 exit 0
