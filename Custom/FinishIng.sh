7a04e4bd57657984fee92c319ccfb5f9ddad22af
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-19 19:41:32 +0800

    Update FinishIng.sh

--- Custom/FinishIng.sh
+++ Custom/FinishIng.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sed -i '/coremark/d' /etc/rc.local
-sed -i '/mediaurlbase/d' /etc/rc.local
-sed -i '/FinishIng.sh/d' /etc/rc.local
-rm -rf /etc/FinishIng.sh
 if [[ -f '/etc/webweb.sh' ]]; then
   source /etc/webweb.sh
   reboot -f
 fi
+if [[ -f /mnt/network ]]; then
+  cp -Rf /mnt/network /etc/config/network
+  uci commit luci
+fi
+sed -i '/coremark/d' /etc/rc.local
+sed -i '/mediaurlbase/d' /etc/rc.local
+sed -i '/FinishIng.sh/d' /etc/rc.local
+rm -rf /etc/FinishIng.sh
 exit 0
