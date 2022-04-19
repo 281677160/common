93644193d16ff1fb29a6d24c3e93130acba9f876
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-19 19:38:59 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -703,17 +703,20 @@ chmod 775 $BASE_PATH/etc/webweb.sh
 sed -i '/webweb.sh/d' "$ZZZ_PATH"
 sed -i "/exit 0/i\source /etc/webweb.sh" "$ZZZ_PATH"
 
+sed -i '/FinishIng.sh/d' "$BASE_PATH/etc/rc.local"
+sed -i "/exit 0/i\[[ -f '/etc/FinishIng.sh' ]] && source /etc/FinishIng.sh" "$BASE_PATH/etc/rc.local"
+
 sed -i '/etc\/init.d\/uhttpd\ restart/d' "$BASE_PATH/etc/rc.local"
 sed -i "/exit 0/i\/etc/init.d/uhttpd restart" "$BASE_PATH/etc/rc.local"
 
+sed -i '/etc\/init.d\/dnsmasq\ restart/d' "$BASE_PATH/etc/rc.local"
+sed -i "/exit 0/i\/etc/init.d/dnsmasq restart" "$BASE_PATH/etc/rc.local"
+
+sed -i '/etc\/init.d\/firewall\ restart/d' "$BASE_PATH/etc/rc.local"
+sed -i "/exit 0/i\/etc/init.d/firewall restart" "$BASE_PATH/etc/rc.local"
+
 sed -i '/etc\/init.d\/network\ restart/d' "$BASE_PATH/etc/rc.local"
 sed -i "/exit 0/i\/etc/init.d/network restart" "$BASE_PATH/etc/rc.local"
-
-sed -i '/service dnsmasq restart/d' "$BASE_PATH/etc/rc.local"
-sed -i "/exit 0/i\service dnsmasq restart" "$BASE_PATH/etc/rc.local"
-
-sed -i '/FinishIng.sh/d' "$BASE_PATH/etc/rc.local"
-sed -i "/exit 0/i\[[ -f '/etc/FinishIng.sh' ]] && source /etc/FinishIng.sh" "$BASE_PATH/etc/rc.local"
 }
 
 function Make_defconfig() {
