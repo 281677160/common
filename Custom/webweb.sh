add0654f13318686972aaf690e59806a86c7b4dd
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-20 00:34:27 +0800

    Update webweb.sh

--- Custom/webweb.sh
+++ Custom/webweb.sh
@@ -5,7 +5,7 @@ touch /etc/crontabs/root
 chmod -R 775 /etc/init.d /usr/share
 
 if [[ `grep -c "FinishIng.sh" /etc/rc.local` -eq '0' ]]; then
-  sed -i "/exit 0/i\[[ -f '/etc/FinishIng.sh' ]] && source /etc/FinishIng.sh" "$BASE_PATH/etc/rc.local"
+  sed -i "/exit 0/i\[[ -f '/etc/FinishIng.sh' ]] && source /etc/FinishIng.sh" "/etc/rc.local"
 fi
 
 if [[ -f /etc/networkip ]]; then
