96603a9baed83fa4c35bed3d61a49c1064b1591c
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-27 11:04:07 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -496,7 +496,7 @@ sudo rm -rf $GITHUB_WORKSPACE/amlogic
 
 function Package_Clear() {
 cd ${TARGET_BSGET}
-for X in $(cat "${CLEAR_PATH}" |cut -d '-' -f3- |sed 's/^/*/g'); do
+for X in $(cat "${CLEAR_PATH}" |cut -d '-' -f3- |sed 's/^/*/g' |sed 's/$/*/g'); do
    rm -rf "${X}"
 done
 }
