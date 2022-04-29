d626604c70ee0b724f52a16ddc9060af8415c89c
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-29 16:26:50 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -325,9 +325,12 @@ sed -i '/^$/d' "$HOME_PATH/feeds.conf.default"
 }
 
 function sbin_openwrt() {
-echo "正在执行：给固件增加[openwrt]命令"
-[[ -f $BUILD_PATH/openwrt.sh ]] && cp -Rf $BUILD_PATH/openwrt.sh $BASE_PATH/sbin/openwrt
-chmod 777 $BASE_PATH/sbin/openwrt
+if [[ -f $BUILD_PATH/openwrt.sh ]]; then
+  echo "正在执行：给固件增加[openwrt]命令"
+  [[ ! -d "$BASE_PATH/usr/bin" ]] && mkdir $BASE_PATH/usr/bin
+  cp -Rf $BUILD_PATH/openwrt.sh $BASE_PATH/usr/bin/openwrt
+  chmod 777 $BASE_PATH/usr/bin/openwrt
+fi
 }
 
 function Diy_Lede() {
