9ec751ff75c06f040330d8b4d9591faeb14e4eaf
281677160 <66651997+281677160@users.noreply.github.com>
2022-09-28 11:38:37 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -99,7 +99,7 @@ else
   export INS="sudo apt"
 fi
 ${INS} update -y
-${INS} install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
+${INS} install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc-s1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
 ${INS} autoremove -y --purge
 ${INS} clean
 if [[ ! ${bendi_script} == "1" ]]; then
