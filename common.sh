da24336eb56837cd46e5188d7ae441eb1171cd1b
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-07 18:40:41 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -149,14 +149,14 @@ if [[ "${REPO_BRANCH}" == "master" ]]; then
   echo "SOURCE=Lede" >> $GITHUB_ENV
   echo "LUCI_EDITION=18.06" >> $GITHUB_ENV
   echo "MAINTAIN=Lean's" >> $GITHUB_ENV
-elif [[ "${REPO_BRANCH}" == "22.03" ]]; then
+elif [[ "${REPO_BRANCH}" == "21.02" ]]; then
   echo "ZZZ_PATH=${GITHUB_WORKSPACE}/openwrt/package/default-settings/files/zzz-default-settings" >> $GITHUB_ENV
   if [[ ! -f "${GITHUB_WORKSPACE}/openwrt/package/default-settings/files/zzz-default-settings" ]]; then
     TIME r "上游源码作者修改了zzz-default-settings文件的路径或者名称，找编译脚本的作者及时修改"
     exit 1
   fi
   echo "SOURCE=Lienol" >> $GITHUB_ENV
-  echo "LUCI_EDITION=22.03" >> $GITHUB_ENV
+  echo "LUCI_EDITION=21.02" >> $GITHUB_ENV
   echo "MAINTAIN=Lienol's" >> $GITHUB_ENV
 elif [[ "${REPO_BRANCH}" == "openwrt-18.06" ]]; then
   echo "ZZZ_PATH=${GITHUB_WORKSPACE}/openwrt/package/emortal/default-settings/files/99-default-settings" >> $GITHUB_ENV
@@ -199,7 +199,7 @@ elif [[ "${matrixtarget}" == "Lienol_source" ]]; then
     exit 1
   fi
   export SOURCE="Lienol"
-  export LUCI_EDITION="22.03"
+  export LUCI_EDITION="21.02"
 elif [[ "${matrixtarget}" == "Tianling_source" ]]; then
   export ZZZ_PATH="${HOME_PATH}/package/emortal/default-settings/files/99-default-settings"
   if [[ ! -f "${ZZZ_PATH}" ]]; then
