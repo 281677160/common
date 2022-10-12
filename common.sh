69583774cc484b7fde3b9ca48f6505adc628021c
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-12 16:08:09 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -679,7 +679,7 @@ if [[ `grep -c "CONFIG_PACKAGE_luci-theme-argon=y" ${HOME_PATH}/.config` -eq '1'
 fi
 
 if [[ `grep -c "CONFIG_PACKAGE_luci-theme-argon=y" ${HOME_PATH}/.config` -eq '1' ]] && [[ `grep -c "CONFIG_TARGET_x86=y" ${HOME_PATH}/.config` -eq '1' ]]; then
-  sed -i '/luci-app-argon-config/d' "${HOME_PATH}/.config"
+  sed -i '/argon-config/d' "${HOME_PATH}/.config"
   sed -i '/argon=y/i\CONFIG_PACKAGE_luci-app-argon-config=y' "${HOME_PATH}/.config"
 fi
 
