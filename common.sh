22527843154172fadea5f48e72b3e45d7ffeb4f4
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-17 03:31:27 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1015,8 +1015,8 @@ TIME r "如果您的机子在线更新固件可用，而又编译了，也可请
 TIME r ""
 TIME r ""
 TIME g "您现在编译所用的服务器CPU型号为[ ${Model_Name} ]"
-TIME g "在github使用核心数为[ ${Cpu_Cores} ],线程数为[ $(nproc) ]"
-TIME g "在github使用内存为[ ${RAM_total} ],现内存剩余为[ ${RAM_available} ]"
+TIME g "在此服务器分配核心数为[ ${Cpu_Cores} ],线程数为[ $(nproc) ]"
+TIME g "在此服务器分配内存为[ ${RAM_total} ],现内存剩余为[ ${RAM_available} ]"
 TIME r ""
 }
 
