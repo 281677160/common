7d41e7eee4ac3f963fb9baf93f008748e4fbccb6
281677160 <66651997+281677160@users.noreply.github.com>
2022-10-26 14:55:18 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -98,7 +98,15 @@ if [[ ! ${bendi_script} == "1" ]]; then
 else
   export INS="sudo apt"
 fi
-sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
+${INS} update -y
+${INS} full-upgrade -y
+${INS} install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+  bzip2 ccache cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib \
+  git gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
+  libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
+  mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip python3-ply \
+  python-docutils qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip \
+  vim wget xmlto xxd zlib1g-dev
 ${INS} install -y rename
 ${INS} autoremove -y --purge
 ${INS} clean
