1c04ccfebb3a92588bc0b34043a63e63bec001b8
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-14 11:04:48 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -2090,7 +2090,6 @@ fi
 
 echo "开始打包"
 cd ${GITHUB_WORKSPACE}/amlogic
-sed -i '/The source_codename/aecho "source_codename=${source_codename}" >> ${GITHUB_ENV}' "${GITHUB_WORKSPACE}/amlogic/make"
 sudo chmod +x make
 if [[ -z "${gh_token}" ]]; then
   sudo ./make -b ${amlogic_model} -k ${amlogic_kernel} -a ${auto_kernel} -s ${rootfs_size} -r ${kernel_repo} -u ${kernel_usage}
