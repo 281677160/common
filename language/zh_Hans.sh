873c204ff27de1fa7b46c5ef206cd46d2a182c9f
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-26 03:43:25 +0800

    Update zh_Hans.sh

--- language/zh_Hans.sh
+++ language/zh_Hans.sh
@@ -5,12 +5,12 @@
 # Convert translation files zh-cn to zh_Hans
 # The script is still in testing, welcome to report bugs.
 
-for X in $(find . -type l -name 'zh-cn' |grep po |grep -v "openclash\|store"); do rm -rf "${X}"; done
-for X in $(find . -type f -name 'zh-cn' |grep po |grep -v "openclash\|store"); do rm -rf "${X}"; done
-for X in $(find . -type l -name 'zh_Hans' |grep po |grep -v "openclash\|store"); do rm -rf "${X}"; done
-for X in $(find . -type f -name 'zh_Hans' |grep po |grep -v "openclash\|store"); do rm -rf "${X}"; done
+for X in $(find . -type l -name 'zh-cn' |grep po |grep -v "openclash\|store\|settings"); do rm -rf "${X}"; done
+for X in $(find . -type f -name 'zh-cn' |grep po |grep -v "openclash\|store\|settings"); do rm -rf "${X}"; done
+for X in $(find . -type l -name 'zh_Hans' |grep po |grep -v "openclash\|store\|settings"); do rm -rf "${X}"; done
+for X in $(find . -type f -name 'zh_Hans' |grep po |grep -v "openclash\|store\|settings"); do rm -rf "${X}"; done
 
-po_file="$({ find |grep -E "[a-z0-9]+\.zh\-cn.+po" |grep -v "openclash\|store"; } 2>"/dev/null")"
+po_file="$({ find |grep -E "[a-z0-9]+\.zh\-cn.+po" |grep -v "openclash\|store\|settings"; } 2>"/dev/null")"
 for a in ${po_file}
 do
 	[ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
@@ -18,7 +18,7 @@ do
 	mv "$a" "${po_new_file}" 2>"/dev/null"
 done
 
-po_file2="$({ find |grep "/zh-cn/" |grep "\.po" |grep -v "openclash\|store"; } 2>"/dev/null")"
+po_file2="$({ find |grep "/zh-cn/" |grep "\.po" |grep -v "openclash\|store\|settings"; } 2>"/dev/null")"
 for b in ${po_file2}
 do
 	[ -n "$(grep "Language: zh_CN" "$b")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$b"
@@ -26,21 +26,21 @@ do
 	mv "$b" "${po_new_file2}" 2>"/dev/null"
 done
 
-lmo_file="$({ find |grep -E "[a-z0-9]+\.zh_Hans.+lmo" |grep -v "openclash\|store"; } 2>"/dev/null")"
+lmo_file="$({ find |grep -E "[a-z0-9]+\.zh_Hans.+lmo" |grep -v "openclash\|store\|settings"; } 2>"/dev/null")"
 for c in ${lmo_file}
 do
 	lmo_new_file="$(echo -e "$c"|sed "s/zh_Hans/zh-cn/g")"
 	mv "$c" "${lmo_new_file}" 2>"/dev/null"
 done
 
-lmo_file2="$({ find |grep "/zh_Hans/" |grep -v "openclash\|store" |grep "\.lmo"; } 2>"/dev/null")"
+lmo_file2="$({ find |grep "/zh_Hans/" |grep -v "openclash\|store\|settings" |grep "\.lmo"; } 2>"/dev/null")"
 for d in ${lmo_file2}
 do
 	lmo_new_file2="$(echo -e "$d"|sed "s/zh_Hans/zh-cn/g")"
 	mv "$d" "${lmo_new_file2}" 2>"/dev/null"
 done
 
-po_dir="$({ find |grep "/zh-cn" |grep -v "openclash\|store" |sed "/\.po/d" |sed "/\.lmo/d"; } 2>"/dev/null")"
+po_dir="$({ find |grep "/zh-cn" |grep -v "openclash\|store\|settings" |sed "/\.po/d" |sed "/\.lmo/d"; } 2>"/dev/null")"
 for e in ${po_dir}
 do
 	po_new_dir="$(echo -e "$e"|sed "s/zh-cn/zh_Hans/g")"
