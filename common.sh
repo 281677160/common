508d1e40c5f5c49997477d1ad054ce753b926cc6
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-01 03:39:05 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -700,7 +700,7 @@ if [[ -f "${ZZZ_PATH}" ]]; then
   sed -i '/exit 0/d' "${ZZZ_PATH}"
 fi
 
-ttydjso="$({ find |grep -E "luci-app-ttyd.json"; } 2>"/dev/null")"
+ttydjso="$({ find |grep -E "luci-app-ttyd.json" |grep -v 'dir' |grep menu.d; } 2>"/dev/null")"
 if [[ -f "${ttydjso}" ]]; then
   curl -fsSL https://raw.githubusercontent.com/281677160/common/main/IMMORTALWRT/ttyd/luci-app-ttyd.json -o "${ttydjso}"
 fi
