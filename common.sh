9b62f6be21536112d3a9ca692cc66e62a815f48d
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-08 00:53:54 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -606,6 +606,7 @@ openwrt-21.02)
   source ${HOME_PATH}/build/common/Share/19.07/netsupport.sh
 ;;
 openwrt-19.07)
+  find . -type d -name "luci-app-natter" -o -name "natter" |grep 'danshui' |xargs -i rm -rf {}
   source ${HOME_PATH}/build/common/Share/19.07/netsupport.sh
   rm -rf ${HOME_PATH}/feeds/packages/libs/libcap && cp -Rf ${HOME_PATH}/build/common/Share/libcap ${HOME_PATH}/feeds/packages/libs/libcap
 ;;
