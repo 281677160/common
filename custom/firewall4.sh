d79e5c2027b8527037d6643eb8f2ab27b1c16c2d
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-14 15:26:20 +0800

    Update firewall4.sh

--- custom/firewall4.sh
+++ custom/firewall4.sh
@@ -27,14 +27,10 @@ cp -rf ./Fullconenat/immortalwrt/package/libs/libnftnl ./package/libs/libnftnl
 rm -rf ./package/network/utils/nftables
 cp -rf ./Fullconenat/immortalwrt/package/network/utils/nftables ./package/network/utils/nftables
 # FW3
-rm -rf ./package/network/config/firewall
-cp -rf ./Fullconenat/openwrt_23/package/network/config/firewall ./package/network/config/firewall
 mkdir -p package/network/config/firewall/patches
 cp -rf ./Fullconenat/immortalwrt_21/package/network/config/firewall/patches/100-fullconenat.patch ./package/network/config/firewall/patches/100-fullconenat.patch
 cp -rf ./Fullconenat/lede/package/network/config/firewall/patches/101-bcm-fullconenat.patch ./package/network/config/firewall/patches/101-bcm-fullconenat.patch
 # iptables
-rm -rf ./package/network/utils/iptables
-cp -rf ./Fullconenat/openwrt_23/package/network/utils/iptables ./package/network/utils/iptables
 cp -rf ./Fullconenat/lede/package/network/utils/iptables/patches/900-bcm-fullconenat.patch ./package/network/utils/iptables/patches/900-bcm-fullconenat.patch
 # network
 wget -qO - https://github.com/openwrt/openwrt/commit/bbf39d07.patch | patch -p1
