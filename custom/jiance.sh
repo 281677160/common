e747b7ed5bb2aeaed968c4bf369607e5dd7e2ab3
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-27 00:31:03 +0800

    Update jiance.sh

--- custom/jiance.sh
+++ custom/jiance.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [[ ! -d "build" ]]; then
-  echo "根目录缺少build文件夹"
+  echo "根目录缺少build文件夹存在"
   exit 1
 fi
 
 if [[ ! -d "build/${FOLDER_NAME}" ]]; then
-  echo "build文件夹内缺少${FOLDER_NAME}文件夹"
+  echo "build文件夹内缺少${FOLDER_NAME}文件夹存在"
   exit 1
 fi
+
+git clone https://github.com/281677160/common build/common
+mv -f build/common/upgrade.sh build/${FOLDER_NAME}/upgrade.sh
+mv -f build/common/xiugai.sh build/${FOLDER_NAME}/common.sh
+chmod -R +x build
