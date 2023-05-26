1a701aa622f6eacdab39e846fcf697b37fa62237
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-26 18:50:18 +0800

    Update xiugai.sh

--- xiugai.sh
+++ xiugai.sh
@@ -209,17 +209,17 @@ else
   GIT_BUILD=build/${FOLDER_NAME}
 fi
 
-if [ -z "$(ls -A "${GITHUB_WORKSPACE}/${GIT_BUILD}/seed/${CONFIG_FILE}" 2>/dev/null)" ]; then
-  TIME r "错误提示：编译脚本的[seed文件夹内缺少${CONFIG_FILE}名称]的配置文件,请在[${GIT_BUILD}/seed]文件夹内补齐"
+if [ -z "$(ls -A "${GITHUB_WORKSPACE}/${GIT_BUILD}/${CONFIG_FILE}" 2>/dev/null)" ]; then
+  TIME r "错误提示：编译脚本的[${FOLDER_NAME}文件夹内缺少${CONFIG_FILE}名称的配置文件],请在[${FOLDER_NAME}/seed]文件夹内补齐"
   exit 1
 else
-  echo "${GIT_BUILD}/seed/${CONFIG_FILE}文件存在"
+  echo "${FOLDER_NAME}/${CONFIG_FILE}文件存在"
 fi
 if [ -z "$(ls -A "${GITHUB_WORKSPACE}/${GIT_BUILD}/${DIY_PART_SH}" 2>/dev/null)" ]; then
-  TIME r "错误提示：编译脚本缺少[${DIY_PART_SH}]名称的自定义设置文件,请在[${GIT_BUILD}]文件夹内补齐"
+  TIME r "错误提示：编译脚本的[${FOLDER_NAME}文件夹内缺少${DIY_PART_SH}名称的自定义设置文件],请在[${FOLDER_NAME}]文件夹内补齐"
   exit 1
 else
-  echo "${GIT_BUILD}/${DIY_PART_SH}文件存在"
+  echo "${FOLDER_NAME}/${DIY_PART_SH}文件存在"
 fi
 }
 
