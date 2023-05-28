92c923cc17eeb0f3ab94f1fd9a11c0f77c805fb9
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-28 21:16:48 +0800

    Update xiugai.sh

--- xiugai.sh
+++ xiugai.sh
@@ -219,13 +219,13 @@ if [ -z "$(ls -A "${GITHUB_WORKSPACE}/${GIT_BUILD}/${CONFIG_FILE}" 2>/dev/null)"
   TIME r "错误提示：编译脚本的[${FOLDER_NAME}文件夹内缺少${CONFIG_FILE}名称的配置文件],请在[${FOLDER_NAME}/seed]文件夹内补齐"
   exit 1
 else
-  echo "${GIT_BUILD}/${CONFIG_FILE}文件存在"
+  TIME g "${GIT_BUILD}/${CONFIG_FILE}文件存在"
 fi
 if [ -z "$(ls -A "${GITHUB_WORKSPACE}/${GIT_BUILD}/${DIY_PART_SH}" 2>/dev/null)" ]; then
   TIME r "错误提示：编译脚本的[${FOLDER_NAME}文件夹内缺少${DIY_PART_SH}名称的自定义设置文件],请在[${FOLDER_NAME}]文件夹内补齐"
   exit 1
 else
-  echo "${GIT_BUILD}/${DIY_PART_SH}文件存在"
+  TIME g "${GIT_BUILD}/${DIY_PART_SH}文件存在"
 fi
 }
 
