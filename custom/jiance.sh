64f50fce639cdcfe8890ae03718aa806d930e96e
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-28 10:49:46 +0800

    Update jiance.sh

--- custom/jiance.sh
+++ custom/jiance.sh
@@ -5,7 +5,7 @@
 cd ${GITHUB_WORKSPACE}
 
 function Diy_continue() {
-rm -rf upcommon
+[[ -d "upcommon" ]] && rm -rf upcommon
 sudo rm -rf build/common && git clone -b main --depth 1 https://github.com/281677160/common build/common
 cp -Rf build/common/*.sh build/${FOLDER_NAME}/
 cp -Rf build/common/xiugai.sh build/${FOLDER_NAME}/common.sh
