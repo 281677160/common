f97d231b96e0d4804874b69591fdf62756cb45e7
281677160 <66651997+281677160@users.noreply.github.com>
2023-06-03 20:59:20 +0800

    Update first.sh

--- custom/first.sh
+++ custom/first.sh
@@ -7,7 +7,7 @@ cd ${GITHUB_WORKSPACE}
 function Diy_continue() {
 cd ${GITHUB_WORKSPACE}
 if [[ -n "${BENDI_VERSION}" ]]; then
-  sudo rm -rf build
+  sudo rm -rf build common.sh
 else
   sudo rm -rf build/common && git clone -b main --depth 1 https://github.com/281677160/common build/common
   cp -Rf build/common/*.sh build/${FOLDER_NAME}/
