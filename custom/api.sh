2120b4b0213ac7e61838f3a39a531da0b4ed1bd5
281677160 <66651997+281677160@users.noreply.github.com>
2023-06-11 15:14:58 +0800

    Update api.sh

--- custom/api.sh
+++ custom/api.sh
@@ -10,7 +10,7 @@
             -H "Accept: application/vnd.github+json" \
             -H "Authorization: Bearer ${REPO_TOKEN}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
-            "https://api.github.com/repos/stupidloud/nanopi-openwrt/actions/runs?per_page=${github_per_page}&page=${github_page}")
+            "https://api.github.com/repos/stupidloud/nanopi-openwrt/actions/runs?&page=100&per_page=100")
 
         # Check if the response is empty or an error occurred
         if [ -z "${response}" ] || [[ "${response}" == *"Not Found"* ]]; then
