8785e988e27e4384a8096eaf56ff5fbd80d077d5
281677160 <66651997+281677160@users.noreply.github.com>
2023-08-31 00:34:17 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -361,10 +361,10 @@ esac
 rm -rf ${HOME_PATH}/feeds/helloworld/{v2ray-core,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
 
 # 更换golang版本
-if [[ -d "${HOME_PATH}/build/common/Share/golang" ]]; then
-  rm -rf ${HOME_PATH}/feeds/packages/lang/golang
-  cp -Rf ${HOME_PATH}/build/common/Share/golang ${HOME_PATH}/feeds/packages/lang/golang
-fi
+# if [[ -d "${HOME_PATH}/build/common/Share/golang" ]]; then
+#   rm -rf ${HOME_PATH}/feeds/packages/lang/golang
+#   cp -Rf ${HOME_PATH}/build/common/Share/golang ${HOME_PATH}/feeds/packages/lang/golang
+# fi
 
 if [[ -d "${HOME_PATH}/feeds/danshui1/relevance/shadowsocks-libev" ]]; then
   rm -rf ${HOME_PATH}/feeds/packages/net/shadowsocks-libev
