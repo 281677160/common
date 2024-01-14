54568d8120b8eb20c44787331832a0b9d34bd134
ShmilyStar <92926599+ShmilyStar@users.noreply.github.com>
2024-01-14 08:59:39 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,7 +6,7 @@
 
 function Diy_Part1() {
 	find . -type d -name 'luci-app-autoupdate' | xargs -i rm -rf {}
-	git clone -b main https://github.com/281677160/luci-app-autoupdate $HOME_PATH/package/luci-app-autoupdate 2>/dev/null
+	git clone -b main https://github.com/ShmilyStar/luci-app-autoupdate $HOME_PATH/package/luci-app-autoupdate 2>/dev/null
 	if [[ `grep -c "luci-app-autoupdate" ${HOME_PATH}/include/target.mk` -eq '0' ]]; then
 		sed -i 's?DEFAULT_PACKAGES:=?DEFAULT_PACKAGES:=luci-app-autoupdate luci-app-ttyd ?g' ${HOME_PATH}/include/target.mk
 	fi
