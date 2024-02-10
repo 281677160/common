c1dc690490773b107709742027c5db2903eab8a4
281677160 <66651997+281677160@users.noreply.github.com>
2024-02-10 11:13:55 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -317,17 +317,14 @@ LIENOL)
     find . -type d -name "${i}" |grep -v 'danshui' |xargs -i rm -rf {}; \
   done
   find . -type d -name "mt" -o -name "pdnsd-alt" -o -name "autosamba" |grep 'other' |xargs -i rm -rf {}
-  if [[ "${REPO_BRANCH}" == "master" ]]; then
-    find . -type d -name "automount" |grep 'other' |xargs -i rm -rf {}
-  elif [[ "${REPO_BRANCH}" == "19.07" ]]; then
-    s="luci-app-vssr,lua-maxminddb,automount,mentohust,luci-app-unblockneteasemusic"
+  if [[ "${REPO_BRANCH}" == "19.07" ]]; then
+    s="mentohust,luci-app-unblockneteasemusic,luci-app-vssr,lua-maxminddb"
     c=(${s//,/ })
     for i in ${c[@]}; do \
       find . -type d -name "${i}" |grep -v 'danshui' |xargs -i rm -rf {}; \
     done
+    find . -type d -name "luci-app-unblockneteasemusic" |xargs -i rm -rf {}
     rm -rf ${HOME_PATH}/feeds/packages/libs/libcap && cp -Rf ${HOME_PATH}/build/common/Share/libcap ${HOME_PATH}/feeds/packages/libs/libcap
-  elif [[ "${REPO_BRANCH}" == "21.02" ]]; then
-    find . -type d -name "automount" |grep 'danshui' |xargs -i rm -rf {}
   elif [[ "${REPO_BRANCH}" == "23.05" ]]; then
     sed -i 's/CONFIG_WERROR=y/# CONFIG_WERROR is not set/g' ${HOME_PATH}/target/linux/generic/config-5.15
   fi
