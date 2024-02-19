f496d4220625ab8c9704594fe5f89894ca977a88
281677160 <66651997+281677160@users.noreply.github.com>
2024-02-19 22:18:08 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -119,7 +119,7 @@ COOLSNOWWOLF)
   export SOURCE="Lede"
   export SOURCE_OWNER="Lean's"
   export LUCI_EDITION="18.06"
-  export DIY_WORK="${FOLDER_NAME}$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
+  export DIY_WORK="${FOLDER_NAME}master"
 ;;
 LIENOL)
   export REPO_URL="https://github.com/Lienol/openwrt"
