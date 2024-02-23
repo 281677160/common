84a405616d5e8232f8055e994a4c8be425c191f1
281677160 <66651997+281677160@users.noreply.github.com>
2024-02-23 22:11:13 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -5,6 +5,9 @@
 
 function install_mustrelyon(){
 # 安装依赖
+docker rmi `docker images -q`
+sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /usr/lib/jvm /opt/ghc /swapfile
+# sudo -E apt-get -qq remove -y --purge azure-cli ghc* zulu* llvm* firefox google* powershell openjdk* msodbcsql17 mongodb* moby* snapd* mysql*
 sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
 sudo apt-get install -y rename pigz libfuse-dev upx subversion clang
 sudo apt-get install -y $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
