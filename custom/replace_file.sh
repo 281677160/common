ddbf5fe0a405cb198d06e362aef348891fd7ed79
281677160 <66651997+281677160@users.noreply.github.com>
2024-03-04 12:47:42 +0800

    Update replace_file.sh

--- custom/replace_file.sh
+++ custom/replace_file.sh
@@ -2,6 +2,7 @@
 # https://github.com/281677160/build-actions
 # replace_file.sh Module by 28677160
 
+function svn_co() {
 if [[ $# -lt 2 ]]; then
   echo "格式错误,正确格式为: [\$svn] [文件夹或文件的链接] [需要替换的文件夹或文件的对应路径],分别以空格分隔"
   return 1
@@ -19,30 +20,23 @@ fssl_link="https://raw.githubusercontent.com/${curl_link}/${branch}/${test}"
 case "${crutch}" in
 blob)
   curl -L "${fssl_link}" -o "${localdir}"
-  if [[ $? -ne 0 ]]; then
-    echo "${rootdir}文件下载失败,请检查网络,或查看链接正确性"
-    return 1
-  else
-    echo "${rootdir}文件替换成功"
-  fi
+  [[ $? -ne 0 ]] && echo "${rootdir}文件下载失败,请检查网络,或查看链接正确性" && return 1
 ;;
 tree)
   tmpdir="$(mktemp -d)" || exit 1
   trap 'rm -rf "${tmpdir}"' EXIT
   git clone -b "${branch}" --depth 1 --filter=blob:none --sparse "${house_link}" "${tmpdir}"
+  [[ $? -ne 0 ]] && echo "${rootdir}文件下载失败,请检查网络,或查看链接正确性" && return 1
   cd "${tmpdir}"
   git sparse-checkout init --cone
   git sparse-checkout set "${test}"
-  if [[ $? -ne 0 ]]; then
-    echo "${rootdir}文件夹下载失败,请检查网络,或查看链接正确性"
-    return 1
-   else
-    echo "${rootdir}文件夹替换成功"
-  fi
   sudo rm -rf "${localdir}" && cp -Rf "${test}" "${localdir}"
   cd "${HOME_PATH}" && sudo rm -rf "${tmpdir}"
 ;;
 *)
-  echo "${rootdir}替换文件操作失败,请保证链接正确性"
+  echo "操作失败,请确保链接正确性"
+  return 1
 ;;
 esac
+}
+svn_co "$@"
