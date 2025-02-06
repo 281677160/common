b3e745160b946e3a003da29cf86bcc53a5d2d16e
clion007 <clion007@126.com>
2025-02-06 22:41:55 +0800

    Update golang version for passwall

--- common.sh
+++ common.sh
@@ -484,7 +484,7 @@ for X in $(ls -1 "${HOME_PATH}/feeds/passwall3"); do
 done
 # 更换golang版本
 rm -rf ${HOME_PATH}/feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x ${HOME_PATH}/feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x ${HOME_PATH}/feeds/packages/lang/golang
 
 if [[ -d "${HOME_PATH}/feeds/danshui1/relevance/shadowsocks-libev" ]]; then
   rm -rf ${HOME_PATH}/feeds/packages/net/shadowsocks-libev
