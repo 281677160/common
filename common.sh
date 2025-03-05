24d2ff530291f95cca5d6ec9c14c8cf1f6bbc21d
clion007 <clion007@126.com>
2025-03-05 19:16:35 +0800

    Update to get golang version automaticlly by Clion

--- common.sh
+++ common.sh
@@ -484,7 +484,8 @@ for X in $(ls -1 "${HOME_PATH}/feeds/passwall3"); do
 done
 # 更换golang版本
 rm -rf ${HOME_PATH}/feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x ${HOME_PATH}/feeds/packages/lang/golang
+$goversion=$(wget -qO- -t1 -T2 https://go.dev/doc/devel/release | grep "released" | awk 'NR==1 {print}' | awk '{printf $2}'| awk -F "." '{printf $2}')
+git clone https://github.com/sbwml/packages_lang_golang -b ${goversion}.x ${HOME_PATH}/feeds/packages/lang/golang
 
 if [[ -d "${HOME_PATH}/feeds/danshui1/relevance/shadowsocks-libev" ]]; then
   rm -rf ${HOME_PATH}/feeds/packages/net/shadowsocks-libev
