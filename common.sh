fe1e68e75dbcefe5443b8d24b02d1d140c546ae9
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-11 12:00:36 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -267,7 +267,7 @@ sed -i "2isrc-git danshui https://github.com/281677160/openwrt-package.git;$SOUR
 
 ./scripts/feeds update -a > /dev/null 2>&1
 
-z="v2ray-core,v2ray-plugin,v2raya,xray-core,xray-plugin"
+z="v2ray-core,v2ray-plugin,v2raya,xray-core,xray-plugin,v2ray-geodata"
 t=(${z//,/ })
 for x in ${t[@]}; do \
   find . -type d -name "${x}" |grep -v 'danshui' |xargs -i rm -rf {}; \
