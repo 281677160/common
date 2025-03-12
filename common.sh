f4dddeaed2f110a5356cf7af805497fa816b0251
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-12 12:20:07 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -684,6 +684,7 @@ if [[ "${Customized_Information}" == "0" ]] || [[ -z "${Customized_Information}"
   echo "不进行,个性签名设置"
 elif [[ -n "${Customized_Information}" ]]; then
   sed -i "s?DESCRIPTION=.*?DESCRIPTION='OpenWrt '\" >> /etc/openwrt_release?g" "${ZZZ_PATH}"
+  sed -i "s?RELEASE=.*?RELEASE='OpenWrt '\" >> /etc/openwrt_release?g" "${ZZZ_PATH}"
   sed -i "s?OpenWrt ?${Customized_Information} @ OpenWrt ?g" "${ZZZ_PATH}"
   echo "个性签名[${Customized_Information}]增加完成"
 fi
