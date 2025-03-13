a077c4adf0c398b5d2b071e8fd8c54cd29c420c5
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-14 03:39:08 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -5,31 +5,20 @@
 
 function install_mustrelyon(){
 # 安装依赖
-sudo mount -o remount,rw /
-fsck -f /
-sudo dpkg --configure -a
-sudo apt-get install -f
 #sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
-sudo apt update -y
-sudo apt full-upgrade -y
-sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+sudo apt-get update -y
+sudo apt-get full-upgrade -y
+sudo apt-get install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 bzip2 ccache clang cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-multilib gettext \
 genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
 libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev \
 libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full patch pkgconf \
 python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion \
-swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
 
-sudo apt-get install -y rename pigz libfuse-dev upx subversion clang
+sudo apt-get install -y rename pigz libfuse-dev upx upx-ucl subversion clang cmdtest
 sudo apt-get install -y $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
 
-sudo rm -rf /usr/local/go
-sudo apt-get -y remove golang
-sudo apt-get -y remove golang-go
-sudo wget https://golang.google.cn/dl/go1.24.1.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 sudo apt-get install -y gcc-13
@@ -55,11 +44,9 @@ function main(){
 		echo "开始升级ubuntu插件和安装依赖....."
 		install_mustrelyon
 		update_apt_source
-		Delete_po2lmo
 	else
 		INS="sudo -E apt-get -qq"
 		install_mustrelyon
-		Delete_useless
 		update_apt_source
 	fi
 }
