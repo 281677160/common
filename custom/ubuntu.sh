2b95a8f497cbd185f5c5fd7d5491fd13926a1b72
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-14 03:41:09 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -4,7 +4,7 @@
 # Copyright (C) ImmortalWrt.org
 
 function install_mustrelyon(){
-# 安装依赖
+# 安装编译openwrt的依赖
 #sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
 sudo apt-get update -y
 sudo apt-get full-upgrade -y
@@ -16,9 +16,11 @@ libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full
 python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion \
 swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
 
+# N1打包需要的依赖
 sudo apt-get install -y rename pigz libfuse-dev upx upx-ucl subversion clang cmdtest
 sudo apt-get install -y $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
 
+# 安装gcc-13
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 sudo apt-get install -y gcc-13
