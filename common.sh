8262dbb9d7d70945baf4a4bb78651ee34ef64a23
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-14 12:33:26 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -289,7 +289,8 @@ gitcon https://github.com/sbwml/feeds_packages_lang_node-prebuilt ${HOME_PATH}/f
 echo '#!/bin/bash' > "${DELETE}" && sudo chmod +x "${DELETE}"
 
 if [[ "${REPO_BRANCH}" == *"18.06"* ]] || [[ "${REPO_BRANCH}" == *"19.07"* ]] || [[ "${REPO_BRANCH}" == *"21.02"* ]] || [[ "${REPO_BRANCH}" == *"22.03"* ]] || [[ "${REPO_BRANCH}" == *"23.05"* ]]; then
-  gitsvn https://github.com/281677160/common/tree/main/Share/shadowsocks-rust ${HOME_PATH}/feeds/danshui/luci-app-ssr-plus/shadowsocks-rust
+  giturl https://github.com/281677160/common/blob/9e73637953026dcb94fea245321e63a15e66320c/Share/shadowsocks-rust/Makefile ${HOME_PATH}/feeds/danshui/luci-app-ssr-plus/shadowsocks-rust/Makefile
+  giturl https://github.com/281677160/common/blob/9e73637953026dcb94fea245321e63a15e66320c/Share/shadowsocks-rust/Makefile ${HOME_PATH}/feeds/danshui/relevance/passwall-packages/shadowsocks-rust/Makefile
   gitsvn https://github.com/immortalwrt/packages/tree/master/lang/rust ${HOME_PATH}/feeds/packages/lang/rust
   source ${HOME_PATH}/build/common/Share/19.07/netsupport.sh
 fi
