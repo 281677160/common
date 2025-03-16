e8d63f469eb61609c16c3afb6b3a8ccd4f757e28
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-16 15:07:44 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -308,6 +308,7 @@ source ${HOME_PATH}/build/common/Share/19.07/netsupport.sh
 
 if [[ "${REPO_BRANCH}" == *"18.06"* ]] || [[ "${REPO_BRANCH}" == *"19.07"* ]] || [[ "${REPO_BRANCH}" == *"21.02"* ]]; then
   gitsvn https://github.com/281677160/common/tree/main/Share/v2raya ${HOME_PATH}/feeds/danshui/luci-app-ssr-plus/v2raya
+  gitsvn https://github.com/immortalwrt/packages/tree/openwrt-23.05/lang/rust ${HOME_PATH}/feeds/packages/lang/rust
 fi
 if [[ "${REPO_BRANCH}" == *"18.06"* ]] || [[ "${REPO_BRANCH}" == *"19.07"* ]] || [[ "${REPO_BRANCH}" == *"21.02"* ]] || [[ "${REPO_BRANCH}" == *"22.03"* ]]; then
     rm -rf ${HOME_PATH}/feeds/danshui/luci-app-nikki
