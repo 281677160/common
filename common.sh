153c310e8c76ff559a0da63c60f3d43793e03491
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-17 10:31:47 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -309,7 +309,7 @@ git pull
  luci-app-eqos,adguardhome,luci-app-adguardhome,mosdns,luci-app-mosdns,luci-app-openclash, \
  luci-app-gost,gost,luci-app-smartdns,smartdns,luci-app-wizard,luci-app-msd_lite,msd_lite, \
  luci-app-ssr-plus,*luci-app-passwall*,v2dat,v2ray-geodata,luci-app-wrtbwmon,wrtbwmon, \
- luci-app-wechatpush,v2ray-core,v2ray-plugin,v2raya,xray-core,xray-plugin"
+ luci-app-wechatpush,v2ray-core,v2ray-plugin,v2raya,xray-core,xray-plugin,luci-app-alist,alist"
  t=(${z//,/ })
  for x in ${t[@]}; do \
    find . -type d -name "${x}" |grep -v 'danshui\|freifunk' |xargs -i rm -rf {}; \
