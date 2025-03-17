6d53116a93ae87bb993655871d9e5c89578ece88
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-17 20:46:36 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -547,7 +547,7 @@ if [[ "${REPO_BRANCH}" == *"19.07"* ]]; then
   gitsvn https://github.com/281677160/common/tree/main/Share/libcap ${HOME_PATH}/feeds/packages/libs/libcap
   gitsvn https://github.com/coolsnowwolf/packages/tree/master/net/kcptun ${HOME_PATH}/feeds/packages/net/kcptun
   gitsvn https://github.com/openwrt/openwrt/tree/openwrt-22.03/package/utils/bcm27xx-userland ${HOME_PATH}/package/utils/bcm27xx-userland
-  gitsvn https://github.com/openwrt/openwrt/tree/openwrt-24.10/tools/cmake ${HOME_PATH}/tools/cmake
+  gitsvn https://github.com/openwrt/openwrt/tree/openwrt-21.02/tools/cmake ${HOME_PATH}/tools/cmake
   rm -fr ${HOME_PATH}/feeds/danshui/luci-app-kodexplorer
 fi
 if [[ "${REPO_BRANCH}" =~ (openwrt-19.07|openwrt-21.02) ]]; then
