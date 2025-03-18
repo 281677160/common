814f4fb03cbf10cf4ca9ba79eb063a7778b61097
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-18 18:19:58 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -359,7 +359,6 @@ if [[ ! -d "${HOME_PATH}/feeds/packages/devel/packr" ]]; then
 fi
 
 if [[ "${REPO_BRANCH}" == *"18.06"* ]] || [[ "${REPO_BRANCH}" == *"19.07"* ]] || [[ "${REPO_BRANCH}" == *"21.02"* ]] || [[ "${REPO_BRANCH}" == *"22.03"* ]]; then
-  #giturl https://github.com/281677160/common/blob/main/Share/shadowsocks-rust/Makefile ${HOME_PATH}/feeds/danshui/luci-app-ssr-plus/shadowsocks-rust/Makefile
   gitsvn https://github.com/openwrt/packages/tree/openwrt-23.05/lang/rust ${HOME_PATH}/feeds/packages/lang/rust
 fi
 
