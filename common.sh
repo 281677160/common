3b48bddc32a6835c3679d73e9012d646e77f2dfe
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-19 19:59:24 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1616,9 +1616,9 @@ cd ${GITHUB_WORKSPACE}
 start_path="${GITHUB_WORKSPACE}/build/${FOLDER_NAME}/relevance/settings.ini"
 chmod -R +x ${start_path} && source ${start_path}
 if [[ "${CPU_SELECTION}" =~ (E5|false) ]]; then
-  kaisbianyixx="弃用E5-编译"
+  kaisbianyixx="编译"
 else
-  kaisbianyixx="使用${CPU_SELECTION}-编译"
+  kaisbianyixx="${CPU_SELECTION}-编译"
 fi
 git clone https://user:${REPO_TOKEN}@github.com/${GIT_REPOSITORY}.git UPLOAD
 mkdir -p "UPLOAD/build/${FOLDER_NAME}/relevance"
