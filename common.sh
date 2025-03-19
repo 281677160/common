ddff70b5a48652e5fa1746869020d84e2435c25a
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-20 03:05:25 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -308,9 +308,11 @@ if [[ -n "${LUCI_CHECKUT}" ]]; then
 fi
 git pull > /dev/null 2>&1
 
+echo "1"
+
  echo "src-git danshui https://github.com/281677160/openwrt-package.git;$SOURCE" >> feeds.conf.default
  ./scripts/feeds update -a > /dev/null 2>&1
- 
+echo "2"
  z="*luci-theme-argon*,*luci-app-argon-config*,*luci-theme-Butterfly*,*luci-theme-netgear*,*luci-theme-atmaterial*, \
  luci-theme-rosy,luci-theme-darkmatter,luci-theme-infinityfreedom,luci-theme-design,luci-app-design-config, \
  luci-theme-bootstrap-mod,luci-theme-freifunk-generic,luci-theme-opentomato,luci-theme-kucat, \
@@ -322,7 +324,7 @@ git pull > /dev/null 2>&1
  for x in ${t[@]}; do \
    find . -type d -name "${x}" |grep -v 'danshui\|freifunk' |xargs -i rm -rf {}; \
  done
-
+echo "3"
 # 更新golang和node版本
 gitsvn https://github.com/sbwml/packages_lang_golang ${HOME_PATH}/feeds/packages/lang/golang
 gitsvn https://github.com/sbwml/feeds_packages_lang_node-prebuilt ${HOME_PATH}/feeds/packages/lang/node
