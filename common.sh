40c152e96d91950c365ecf9c6d4bbff09435dd53
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-20 03:18:02 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -169,7 +169,7 @@ OFFICIAL)
   export SOURCE="Official"
   export SOURCE_OWNER="openwrt's"
   export LUCI_EDITION="$(echo "${REPO_BRANCH}" |sed 's/openwrt-//g')"
-  export DIY_WORK="padavanonly$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
+  export DIY_WORK="${FOLDER_NAME}$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
   export CON_TENTCOM="$(echo "${REPO_URL}" |cut -d"/" -f4-5)"
   export RAW_WEB="https://raw.githubusercontent.com/${CON_TENTCOM}/${REPO_BRANCH}"
   export FEEDS_CONF="$RAW_WEB/feeds.conf.default"
@@ -177,7 +177,7 @@ OFFICIAL)
 ;;
 PADAVANONLY)
   export REPO_URL="https://github.com/padavanonly/immortalwrt-mt798x-24.10"
-  export SOURCE="MT798x"
+  export SOURCE="Mt798x"
   export SOURCE_OWNER="PADAVANONLY's"
   export LUCI_EDITION="$(echo "${REPO_BRANCH}" |sed 's/openwrt-//g')"
   export DIY_WORK="${FOLDER_NAME}$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
