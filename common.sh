86f37e01903806258854883a94d16bf7b4fd0542
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-23 23:02:27 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1153,9 +1153,9 @@ if [[ "${AdGuardHome_Core}" == "1" ]]; then
   echo -e "\nCONFIG_PACKAGE_luci-app-adguardhome=y" >> ${HOME_PATH}/.config
 fi
 
-if ! grep -q "auto-scripts=y" ${HOME_PATH}/.config; then
-  echo -e "\nCONFIG_PACKAGE_auto-scripts=y" >> ${HOME_PATH}/.config
-fi
+#if ! grep -q "auto-scripts=y" ${HOME_PATH}/.config; then
+#  echo -e "\nCONFIG_PACKAGE_auto-scripts=y" >> ${HOME_PATH}/.config
+#fi
 
 if [[ `grep -c "CONFIG_PACKAGE_dnsmasq_full_nftset=y" ${HOME_PATH}/.config` -eq '1' ]]; then
   if [[ `grep -c "CONFIG_PACKAGE_luci-app-passwall2_Nftables_Transparent_Proxy=y" ${HOME_PATH}/.config` -eq '1' ]]; then
