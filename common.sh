afa3f1d59e9d1cc2e65f9ffa521931dd51d4c4f1
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-24 13:03:13 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -323,7 +323,7 @@ fi
 
 # 降低luci-app-ssr-plus的shadowsocks-rust版本
 if [[ "${REPO_BRANCH}" == *"18.06"* ]] || [[ "${REPO_BRANCH}" == *"19.07"* ]] || [[ "${REPO_BRANCH}" == *"21.02"* ]] || [[ "${REPO_BRANCH}" == *"22.03"* ]]; then
-   source ${HOME_PATH}/build/common/Share/19.07/netsupport.sh
+   source ${HOME_PATH}/build/common/Share/tproxy/netsupport.sh
    gitsvn https://github.com/281677160/common/blob/main/Share/shadowsocks-rust/Makefile ${HOME_PATH}/feeds/danshui/luci-app-ssr-plus/shadowsocks-rust/Makefile
    gitsvn https://github.com/281677160/common/blob/main/Share/shadowsocks-rust/Makefile ${HOME_PATH}/feeds/danshui/relevance/passwall-packages/shadowsocks-rust/Makefile
    rm -rf ${HOME_PATH}/feeds/danshui/luci-app-fancontrol
