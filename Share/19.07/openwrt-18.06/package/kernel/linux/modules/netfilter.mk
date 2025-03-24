94f11489392b4bceeda2896afae7c8f6e234880b
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-24 12:16:13 +0800

    Update netfilter.mk

--- Share/19.07/openwrt-18.06/package/kernel/linux/modules/netfilter.mk
+++ Share/19.07/openwrt-18.06/package/kernel/linux/modules/netfilter.mk
@@ -730,6 +730,21 @@ endef
 
 $(eval $(call KernelPackage,ipt-led))
 
+define KernelPackage/ipt-socket
+  TITLE:=Iptables socket matching support
+  DEPENDS+=+kmod-nf-socket +kmod-nf-conntrack
+  KCONFIG:=$(KCONFIG_IPT_SOCKET)
+  FILES:=$(foreach mod,$(IPT_SOCKET-m),$(LINUX_DIR)/net/$(mod).ko)
+  AUTOLOAD:=$(call AutoProbe,$(notdir $(IPT_SOCKET-m)))
+  $(call AddDepends/ipt)
+endef
+
+define KernelPackage/ipt-socket/description
+  Kernel modules for socket matching
+endef
+
+$(eval $(call KernelPackage,ipt-socket))
+
 define KernelPackage/ipt-tproxy
   TITLE:=Transparent proxying support
   DEPENDS+=+kmod-nf-tproxy +kmod-nf-conntrack
@@ -738,12 +753,6 @@ define KernelPackage/ipt-tproxy
   AUTOLOAD:=$(call AutoProbe,$(notdir $(IPT_TPROXY-m)))
   $(call AddDepends/ipt)
 endef
-
-define KernelPackage/ipt-tproxy/description
-  Kernel modules for Transparent Proxying
-endef
-
-$(eval $(call KernelPackage,ipt-tproxy))
 
 define KernelPackage/ipt-tee
   TITLE:=TEE support
