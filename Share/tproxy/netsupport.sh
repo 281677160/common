fb15e1c04e1f7c2c980673329795160293a96c63
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-25 12:43:41 +0800

    Update netsupport.sh

--- Share/tproxy/netsupport.sh
+++ Share/tproxy/netsupport.sh
@@ -103,6 +103,18 @@ fi
 
 
 
+
+   if [[ "${REPO_BRANCH}" == "2410" ]] && [[ "${SOURCE_CODE}" == "PADAVANONLY" ]]; then
+    gitsvn https://github.com/281677160/common/blob/main/Share/tproxy/padavanonly/package/kernel/linux/modules/usb.mk package/kernel/linux/modules/usb.mk
+    gitsvn https://github.com/281677160/common/blob/main/Share/tproxy/padavanonly/package/kernel/linux/modules/crypto.mk package/kernel/linux/modules/crypto.mk
+    gitsvn https://github.com/281677160/common/blob/main/Share/tproxy/padavanonly/package/kernel/linux/modules/hwmon.mk package/kernel/linux/modules/hwmon.mk
+    gitsvn https://github.com/281677160/common/blob/main/Share/tproxy/padavanonly/package/kernel/linux/modules/netsupport.mk package/kernel/linux/modules/netsupport.mk
+    gitsvn https://github.com/immortalwrt/immortalwrt/blob/openwrt-24.10/include/netfilter.mk include/netfilter.mk
+    gitsvn https://github.com/immortalwrt/immortalwrt/tree/openwrt-24.10/package/firmware package/firmware
+  fi
+
+
+
 function gitsvn() {
 cd "${HOME_PATH}"
 local A="${1%.git}"
