1602087dcb977b604fb8f0c3c6a7865cf674c3a3
281677160 <66651997+281677160@users.noreply.github.com>
2025-03-25 21:43:54 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1195,9 +1195,11 @@ if ! grep -q "auto-scripts=y" ${HOME_PATH}/.config; then
   echo -e "\nCONFIG_PACKAGE_auto-scripts=y" >> ${HOME_PATH}/.config
 fi
 
-#if [[ "${REPO_BRANCH}" == *"22.03"* ]]; then
-#  sed -i '/ipv6helper=y/d' "${HOME_PATH}/.config"
-#fi
+if [[ `grep -c "CONFIG_PACKAGE_libopenssl-afalg_sync=y" ${HOME_PATH}/.config` -eq '1' ]]; then
+  if [[ `grep -c "CONFIG_PACKAGE_libopenssl-devcrypto=y" ${HOME_PATH}/.config` -eq '1' ]]; then
+    sed -i 's/CONFIG_PACKAGE_libopenssl-devcrypto=y/# CONFIG_PACKAGE_libopenssl-devcrypto is not set/g' ${HOME_PATH}/.config
+  fi
+fi
 
 if [[ `grep -c "CONFIG_PACKAGE_dnsmasq_full_nftset=y" ${HOME_PATH}/.config` -eq '1' ]]; then
   if [[ `grep -c "CONFIG_PACKAGE_luci-app-passwall2_Nftables_Transparent_Proxy=y" ${HOME_PATH}/.config` -eq '1' ]]; then
