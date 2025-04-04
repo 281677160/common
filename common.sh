0fea1b21ecadc97251ada0956f5934c05c4e9b65
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-05 00:03:48 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -319,7 +319,7 @@ luci-app-ssr-plus,*luci-app-passwall*,v2dat,v2ray-geodata, \
 luci-app-wechatpush,v2ray-core,v2ray-plugin,v2raya,xray-core,xray-plugin,luci-app-alist,alist"
 t=(${z//,/ })
 for x in ${t[@]}; do \
-  find . -path "./danshui" -prune -o -type d -name "$x" -print -exec rm -rf {} \;
+  find ${HOME_PATH} -path "${HOME_PATH}/feeds/danshui" -prune -o -type d -name "$x" -print -exec rm -rf {} \;
 done
 
 if [[ ! "${REPO_BRANCH}" =~ ^(main|master|(openwrt-)?(24\.10))$ ]]; then
