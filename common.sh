5bb68f1961f584098f41bc16c41854301240e253
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-04 23:44:38 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -310,7 +310,9 @@ fi
 echo "src-git danshui https://github.com/281677160/openwrt-package.git;$SOURCE" >> feeds.conf.default
 ./scripts/feeds update -a > /dev/null 2>&1
 
-for i in "luci-theme-argon" "luci-theme-design" "luci-app-design-config" "luci-theme-infinityfreedom" "luci-theme-darkmatter"; do find . -path "./danshui" -prune -o -type d -name "$i" -print done
+for i in "luci-theme-argon" "luci-theme-design" "luci-app-design-config" "luci-theme-infinityfreedom" "luci-theme-darkmatter"; do \
+find . -path "./danshui" -prune -o -type d -name "$i" -print; \
+done
 
 if [[ ! "${REPO_BRANCH}" =~ ^(main|master|(openwrt-)?(24\.10))$ ]]; then
   rm -rf ${HOME_PATH}/feeds/danshui/luci-app-fancontrol
