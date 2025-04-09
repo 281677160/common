5e0161a0e38ae2e7defae451cef51634639e9344
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-09 23:52:50 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -10,47 +10,26 @@ ${INS} update > /dev/null 2>&1
 
 # 安装编译openwrt的依赖
 ${INS} install ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
-bzip2 ccache cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-multilib gettext \
+bzip2 ccache clang cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-multilib gettext \
 genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
 libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev \
 libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full patch pkgconf \
 python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion \
-swig texinfo uglifyjs unzip upx-ucl vim wget xmlto xxd zlib1g-dev
+swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
 
 # N1打包需要的依赖
-${INS} install rename pigz clang
+${INS} install rename pigz clang upx-ucl
 
-# 安装gcc g++
-GCC_REV="13"
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+# 安装gcc-13
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 ${INS} update > /dev/null 2>&1
-${INS} install gcc-$GCC_REV g++-$GCC_REV
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$GCC_REV 100
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$GCC_REV 100
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
-
-# 安装po2lmo
-${INS} install libncurses-dev libssl-dev libgmp-dev libexpat1-dev python3-pip python3-distutils
-sudo rm -rf po2lmo
-git clone --filter=blob:none --no-checkout "https://github.com/openwrt/luci.git" "po2lmo"
-pushd "po2lmo"
-git config core.sparseCheckout true
-echo "modules/luci-base/src" >> ".git/info/sparse-checkout"
-git checkout
-cd "modules/luci-base/src"
-sudo make po2lmo
-sudo strip "po2lmo"
-sudo rm -rf "/usr/bin/po2lmo"
-sudo cp -fp "po2lmo" "/usr/bin/po2lmo"
-popd
-sudo rm -rf po2lmo
-
-
+${INS} install gcc-13
+${INS} install g++-13
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 60 --slave /usr/bin/g++ g++ /usr/bin/g++-13
 gcc --version
 g++ --version
 clang --version
-upx --version
 }
 
 function update_apt_source(){
