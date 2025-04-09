b588a841656822576eee8fe666d1281d35b8aae5
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-09 16:09:36 +0800

    Update yyy.sh

--- yyy.sh
+++ yyy.sh
@@ -9,7 +9,7 @@ function TIME() {
     z) export Color="\e[35m";;
     l) export Color="\e[36m";;
   esac
-echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+echo -e "\e[36m\e[0m${Color}${2}\e[0m"
 }
 
 source /etc/os-release
