30c968fb99c2fae4345ba76238e73c44d3bcb124
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-09 13:12:54 +0800

    Update yyy.sh

--- yyy.sh
+++ yyy.sh
@@ -12,26 +12,25 @@ function TIME() {
 echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
 }
 
-export GITHUB_WORKSPACE="$PWD"
+source /etc/os-release
+if [[ ! "${UBUNTU_CODENAME}" =~ (bionic|focal|jammy) ]]; then
+  TIME r "请使用Ubuntu 22.04 LTS位系统"
+  exit 1
+fi
+
+if [[ $EUID -eq 0 ]]; then
+  TIME r "警告：请勿使用root用户编译，换一个普通用户吧~~"
+  exit 1
+fi
+
+export GITHUB_WORKSPACE="/home/$USER"
 export HOME_PATH="${GITHUB_WORKSPACE}/openwrt"
 export OPERATES_PATH="${GITHUB_WORKSPACE}/operates"
 export GITHUB_ENV="${GITHUB_WORKSPACE}/bendi"
 export CURRENT_PATH="${GITHUB_WORKSPACE##*/}"
 export BENDI_VERSION="1"
 install -m 0755 /dev/null $GITHUB_ENV
-if [[ ! "$USER" == "openwrt" ]] && [[ "${CURRENT_PATH}" == "openwrt" ]]; then
-  TIME r "已在openwrt文件夹内,请在勿在此路径使用一键命令"
-  exit 1
-fi
-source /etc/os-release
-if [[ ! "${UBUNTU_CODENAME}" =~ (bionic|focal|jammy) ]]; then
-  TIME r "请使用Ubuntu 22.04 LTS位系统"
-  exit 1
-fi
-if [[ "$USER" == "root" ]]; then
-  TIME r "警告：请勿使用root用户编译，换一个普通用户吧~~"
-  exit 1
-fi
+
 Google_Check=$(curl -I -s --connect-timeout 8 google.com -w %{http_code} | tail -n1)
 if [ ! "${Google_Check}" == 301 ]; then
   TIME r "提醒：编译之前请自备梯子，编译全程都需要稳定翻墙的梯子~~"
@@ -40,6 +39,8 @@ fi
 if [[ `sudo grep -c "sudo ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers` -eq '0' ]]; then
   sudo sed -i 's?%sudo.*?%sudo ALL=(ALL:ALL) NOPASSWD:ALL?g' /etc/sudoers
 fi
+
+cd $GITHUB_WORKSPACE
 
 function Ben_diskcapacity() {
 Cipan_Size="$(df -hT $PWD|awk 'NR==2'|awk '{print $(3)}')"
