12276be21196f9508844861b7da6866b1ea6bf9e
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-10 13:46:10 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -19,6 +19,10 @@ libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full
 python2.7 python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion \
 swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
 
+${INS} -y install build-essential asciidoc binutils bzip2 curl gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip \
+zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev \
+xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf
+
 # N1打包需要的依赖
 ${INS} install rename pigz clang upx-ucl
 
