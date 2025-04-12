a23b108c3a1949cc08c5e757abdc0420638aa158
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-13 02:40:43 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -537,12 +537,13 @@ if [[ ! -f "${HOME_PATH}/staging_dir/host/bin/upx" ]]; then
 fi
 
 # 正在执行插件语言修改
-if [[ -d "${HOME_PATH}/feeds/luci/modules/luci-mod-system" ]]; then
-  bash <(curl -fsSL https://raw.githubusercontent.com/281677160/common/main/language/zh_Hans.sh)
-else
-  bash <(curl -fsSL https://raw.githubusercontent.com/281677160/common/main/language/zh-cn.sh)
+if [[ ! "${NUM_BER}" == "2" ]]; then
+  if [[ -d "${HOME_PATH}/feeds/luci/modules/luci-mod-system" ]]; then
+    bash <(curl -fsSL https://raw.githubusercontent.com/281677160/common/main/language/zh_Hans.sh)
+  else
+    bash <(curl -fsSL https://raw.githubusercontent.com/281677160/common/main/language/zh-cn.sh)
+  fi
 fi
-
 # files文件夹删除LICENSE,README
 [[ -d "${HOME_PATH}/files" ]] && sudo chmod +x ${HOME_PATH}/files
 rm -rf ${HOME_PATH}/files/{LICENSE,README}
