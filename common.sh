b41adfd92d17eb288a68600ffd39deb9ac8fcddb
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-13 00:46:08 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -217,11 +217,11 @@ fi
 if [[ -d "${HOME_PATH}/SRC_LUCI/modules/luci-mod-system" ]]; then
   THEME_BRANCH="Theme2"
   rm -rf ${HOME_PATH}/SRC_LUCI
-  gitsvn https://github.com/jerrykuku/luci-theme-argon.git ${HOME_PATH}/package/luci-theme-argon
+  gitsvn https://github.com/281677160/luci-theme-argon/tree/master ${HOME_PATH}/package/luci-theme-argon
 else
   THEME_BRANCH="Theme1"
   rm -rf ${HOME_PATH}/SRC_LUCI
-  gitsvn https://github.com/jerrykuku/luci-theme-argon/tree/18.06 ${HOME_PATH}/package/luci-theme-argon
+  gitsvn https://github.com/281677160/luci-theme-argon/tree/18.06 ${HOME_PATH}/package/luci-theme-argon
 fi
 
 echo "src-git danshui https://github.com/281677160/openwrt-package.git;$SOURCE" >> ${HOME_PATH}/feeds.conf.default
