5a05aa171c90da3b230b853ae2057f85fe3549d4
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-14 00:10:53 +0800

    Update zh_Hans.sh

--- language/zh_Hans.sh
+++ language/zh_Hans.sh
@@ -5,7 +5,7 @@
 # The script is still in testing, welcome to report bugs.
 # ------------------------------- Start Conversion -------------------------------
 convert_files=0
-for X in $(find . -regex '.*zh-cn\|.*zh_Hans' -type f |grep po); do rm -rf "${X}"; done
+for X in $(find . -regex '.*zh-cn\|.*zh_Hans' -type f |grep po |grep -v "settings"); do rm -rf "${X}"; done
 
 ha_file="$({ find . -type d -name "zh-cn" | sed 's|/[^/]*$||' | sort -u; } 2>"/dev/null")"
 for y in ${ha_file}
@@ -14,7 +14,7 @@ do
     let convert_files++
 done
 
-po_file="$({ find |grep -E "[a-z0-9]+\.zh\-cn.+po"; } 2>"/dev/null")"
+po_file="$({ find |grep -E "[a-z0-9]+\.zh\-cn.+po" |grep -v "settings"; } 2>"/dev/null")"
 for a in ${po_file}
 do
     [ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/zh_CN/zh_Hans/g" "$a"
@@ -23,7 +23,7 @@ do
     let convert_files++
 done
 
-po_file2="$({ find |grep "/zh-cn/" |grep "\.po"; } 2>"/dev/null")"
+po_file2="$({ find |grep "/zh-cn/" |grep "\.po" |grep -v "settings"; } 2>"/dev/null")"
 for b in ${po_file2}
 do
     [ -n "$(grep "Language: zh_CN" "$b")" ] && sed -i "s/zh_CN/zh_Hans/g" "$b"
@@ -32,14 +32,14 @@ do
     let convert_files++
 done
 
-zh_file="$({ find |grep "/zh_Hans/" |grep "\.po"; } 2>"/dev/null")"
+zh_file="$({ find |grep "/zh_Hans/" |grep "\.po" |grep -v "settings"; } 2>"/dev/null")"
 for h in ${zh_file}
 do
     [ -n "$(grep "Language: zh_CN" "$h")" ] && sed -i "s/zh_CN/zh_Hans/g" "$h"
     let convert_files++
 done
 
-lmo_file="$({ find |grep -E "[a-z0-9]+\.zh_Hans.+lmo"; } 2>"/dev/null")"
+lmo_file="$({ find |grep -E "[a-z0-9]+\.zh_Hans.+lmo" |grep -v "settings"; } 2>"/dev/null")"
 for c in ${lmo_file}
 do
     lmo_new_file="$(echo -e "$c"|sed "s/zh_Hans/zh-cn/g")"
@@ -47,7 +47,7 @@ do
     let convert_files++
 done
 
-lmo_file2="$({ find |grep "/zh_Hans/" |grep "\.lmo"; } 2>"/dev/null")"
+lmo_file2="$({ find |grep "/zh_Hans/" |grep "\.lmo" |grep -v "settings"; } 2>"/dev/null")"
 for d in ${lmo_file2}
 do
     lmo_new_file2="$(echo -e "$d"|sed "s/zh_Hans/zh-cn/g")"
@@ -55,7 +55,7 @@ do
     let convert_files++
 done
 
-po_dir="$({ find |grep "/zh-cn" |sed "/\.po/d" |sed "/\.lmo/d"; } 2>"/dev/null")"
+po_dir="$({ find |grep "/zh-cn" |sed "/\.po/d" |sed "/\.lmo/d" |grep -v "settings"; } 2>"/dev/null")"
 for e in ${po_dir}
 do
     po_new_dir="$(echo -e "$e"|sed "s/zh-cn/zh_Hans/g")"
@@ -63,7 +63,7 @@ do
     let convert_files++
 done
 
-makefile_file="$({ find|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
+makefile_file="$({ find|grep Makefile |sed "/Makefile./d" |grep -v "settings"; } 2>"/dev/null")"
 for f in ${makefile_file}
 do
     [ -n "$(grep "zh-cn" "$f")" ] && sed -i "s/zh-cn/zh_Hans/g" "$f"
