5ca47713f6aa2b453f6f813fcc740e04d3a36949
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-14 21:19:21 +0800

    Update nft_tproxy.sh

--- Share/tproxy/nft_tproxy.sh
+++ Share/tproxy/nft_tproxy.sh
@@ -203,10 +203,9 @@ if [[ "${REPO_BRANCH}" == *"22.03"* ]]; then
 fi
 
 if [[ "${SOURCE_CODE}" == "MT798X" ]] && [[ "${REPO_BRANCH}" =~ (openwrt-21.02|openwrt-23.05) ]]; then
-  git clone --depth=1 https://github.com/281677160/mt798x mt798xmk
+  git clone https://github.com/281677160/mt798x mt798xmk
   rm -rf package/boot && cp -r mt798xmk/package/boot package/boot
   rm -rf target/linux/mediatek && cp -r mt798xmk/target/linux/mediatek target/linux/mediatek
-  rm -rf target/linux/generic/backport-5.4 && cp -r mt798xmk/target/linux/generic/backport-5.4 target/linux/generic/backport-5.4
   rm -rf package/boot/arm-trusted-firmware-stm32
   gitcon https://github.com/hanwckf/immortalwrt-mt798x/tree/openwrt-21.02/package/network/services/hostapd package/network/services/hostapd
 fi
