5455d7ef6c8866ab8ccb9461a273d6b2d333edc4
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-22 19:01:51 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -27,7 +27,7 @@ bzip2 ccache cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-mul
 genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
 libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev \
 libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full patch pkgconf \
-python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools \
+python3.6 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools \
 subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev  python3-distutils
 
 # alist依赖
