85e38edae0a4cd174bf0ae2012da1a60305a3fd5
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-22 22:05:15 +0800

    Update ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -55,8 +55,9 @@ bzip2 ccache cmake cpio curl device-tree-compiler flex gawk gcc-multilib g++-mul
 genisoimage git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev \
 libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev \
 libreadline-dev libssl-dev libtool llvm lrzsz msmtp ninja-build p7zip p7zip-full patch pkgconf \
-python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools \
-subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev python3-distutils
+python3-pip python3-cryptography python3-docutils python3-ply python3-pyelftools python3-requests
+python3-setuptools python3-distutils qemu-utils rsync scons squashfs-tools subversion swig \
+texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 
 # alist依赖
 apt-get install -y libfuse-dev
