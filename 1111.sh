be53ed0168388f6763764b475aa8dae213caf109
281677160 <66651997+281677160@users.noreply.github.com>
2025-05-09 18:06:16 +0800

    Update 1111.sh

--- 1111.sh
+++ 1111.sh
@@ -12,6 +12,9 @@ if [[ "$url" == *"tree"* ]]; then
     base_url="${url%/tree*}"
     echo "base_url: $base_url"
 
+    # 提取账号跟仓库
+    ck_name="$(echo "$base_url" | cut -d"/" -f4-5)"
+
     # 提取tree前面的第一个/值（即仓库名）
     repo_name=$(basename "$base_url")
     echo "repo_name: $repo_name"
@@ -49,12 +52,11 @@ elif [[ "$url" == *"blob"* ]]; then
     path_after_branch="${after_tree#*/}"
     echo "path_after_branch: $path_after_branch"
 
-    # 提取最后一个/后的数值
-    last_part="${path_after_branch##*/}"
-    echo "last_part: $last_part"
+    # 确定最后的下载链接
+    base_url="https://raw.githubusercontent.com/$ck_name/$branch/$path_after_branch"
 
     # 确定文件名称
-    [[ -n "$last_part" ]] && files_name="$last_part" || { echo "错误链接,文件为空"; return; }
+    [[ -n "$path_files_branch" ]] && files_name="$path_files_branch" || { echo "错误链接,文件为空"; return; }
 elif [[ "$url" == *"https://github.com"* ]]; then
     # 不包含tree的情况
     base_url="$url"
@@ -84,6 +86,7 @@ else
 fi
 
 if [[ "$url" == *"tree"* ]] && [[ -n "$path_after_branch" ]]; then
+    [[ "$B" == "all" ]] && path_name="$tmpdir/$path_after_branch/*" || path_name="$tmpdir/$path_after_branch"
     if git clone -q --no-checkout "$base_url" "$tmpdir"; then
         cd "$tmpdir"
         git sparse-checkout init --cone > /dev/null 2>&1
@@ -92,5 +95,31 @@ if [[ "$url" == *"tree"* ]] && [[ -n "$path_after_branch" ]]; then
         # 替换路径中的特定字符串
         grep -rl 'include ../../luci.mk' . | xargs -r sed -i 's#include ../../luci.mk#include \$(TOPDIR)/feeds/luci/luci.mk#g'
         grep -rl 'include ../../lang/' . | xargs -r sed -i 's#include ../../lang/#include \$(TOPDIR)/feeds/packages/lang/#g'
-        
-
+        cd $HOME_PATH
+    else
+        echo "$files_name文件下载失败"
+        return
+    fi
+elif [[ "$url" == *"tree"* ]] && [[ -n "$branch" ]]; then
+    [[ "$B" == "all" ]] && path_name="$tmpdir/*" || path_name="$tmpdir"
+    if ! git clone -q --single-branch --depth=1 --branch=$branch $base_url $tmpdir; then
+        echo "$files_name文件下载失败"
+        return
+    fi
+elif [[ "$url" == *"https://github.com"* ]]; then
+    [[ "$B" == "all" ]] && path_name="$tmpdir/*" || path_name="$tmpdir"
+    if ! git clone -q --depth 1 $base_url $tmpdir; then
+        echo "$files_name文件下载失败"
+        return
+    fi
+elif [[ "$url" == *"blob"* ]]; then
+    parent_dir="${store_away%/*}"
+    [[ ! -d "$parent_dir" ]] && mkdir -p "$parent_dir"
+    if ! curl -fsSL $branch -o $store_away; then
+        echo "$files_name文件下载失败"
+        return
+    fi
+else
+    echo "无效的github链接"
+    return
+fi
