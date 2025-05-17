53435eca4e1fc0afd3ce348c544a0b2d72875e1b
281677160 <66651997+281677160@users.noreply.github.com>
2025-05-17 13:20:28 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1441,17 +1441,15 @@ function Diy_menu6() {
 Diy_variable
 }
 
-case "$1" in
-  "Diy_menu")
-    Diy_menu ;;
-  "Diy_menu2")
-    Diy_menu2 ;;
-  "Diy_menu3")
-    Diy_menu3 ;;
-  "Diy_menu4")
-    Diy_menu4 ;;
-  "Diy_menu5")
-    Diy_menu5 ;;
-  "Diy_menu6")
-    Diy_menu6 ;;
+case "${1:-}" in
+  "Diy_menu") Diy_menu ;;
+  "Diy_menu2") Diy_menu2 ;;
+  "Diy_menu3") Diy_menu3 ;;
+  "Diy_menu4") Diy_menu4 ;;
+  "Diy_menu5") Diy_menu5 ;;
+  "Diy_menu5") Diy_menu6 ;;
+  *) 
+    echo "Usage: $0 {Diy_menu|Diy_menu2|Diy_menu3|Diy_menu4|Diy_menu5}" >&2
+    exit 1
+    ;;
 esac
