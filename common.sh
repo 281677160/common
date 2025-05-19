fa2e5d8ffa1a48bf8585e1faf7e544f9367b22ac
281677160 <66651997+281677160@users.noreply.github.com>
2025-05-19 18:11:44 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -411,7 +411,7 @@ if [[ ! "${Default_theme}" == "0" ]] && [[ -n "${Default_theme}" ]]; then
 fi
 
 # 更新和安装feeds
-./scripts/feeds install -a > /dev/null 2>&1
+./scripts/feeds install -a -q &>/dev/null
 ./scripts/feeds install -a
 
 # 使用自定义配置文件
